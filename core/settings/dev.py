e3f3dd8ade2f9b08bd270ba2f467561b41cbde2b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-11-25 23:39:57 -0500

    g

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -44,3 +44,12 @@ CORS_ORIGIN_WHITELIST = [
     "https://desing.netsy.ai",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    "https://desing.netsy.ai",
+]
+
+
