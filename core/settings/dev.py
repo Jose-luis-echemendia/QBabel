c3a4f3432575eabd2ccc4e014fbe08c77928e4c8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-11-25 23:39:46 -0500

    f

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -35,3 +35,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     "https://desing.netsy.ai",
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    "https://desing.netsy.ai",
+]
+
