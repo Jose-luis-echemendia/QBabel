d9337be92d15a4831712b16ca9b8c725cdea8054
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-11-25 23:39:30 -0500

    f

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -2,6 +2,22 @@ from .base import *
 
 DEBUG = True
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "root": {
+        "handlers": ["file"],
+        "level": "DEBUG",
+    },
+}
+
 ALLOWED_HOSTS = [
     "*",
     "127.0.0.1",
