8ef29e57a00142bfbc80b9cc7c1838fe1f8707bf
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-11-27 23:40:04 -0500

    d

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Documento(models.Model):
+    nombre = models.CharField(max_length=100)
+    archivo = models.FileField(upload_to='documentos/')
+    
+    def __str__(self):
+        return self.nombre
