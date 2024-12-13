d19b0b855de82ae53447aa3502ac67638a61abba
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-13 11:21:34 -0500

    d

--- src/components/load-suspense/index.jsx
+++ src/components/load-suspense/index.jsx
@@ -1,6 +1,11 @@
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import { LoadingSuspense } from './loading-suspense'
+import PropTypes from 'prop-types'
 
 export default function LoadSuspense ({ children }) {
   return <Suspense fallback={<LoadingSuspense />}>{children}</Suspense>
+}
+
+LoadSuspense.propTypes = {
+  children: PropTypes.node.isRequired,
 }
--- src/components/load-suspense/loading-suspense.jsx
+++ src/components/load-suspense/loading-suspense.jsx
@@ -1,13 +1,9 @@
-import styles from '@styles/components/LoadingSuspense.css'
+import styles from '@styles/components/LoadingSuspense.css';
 
 export const LoadingSuspense = () => {
   return (
-    <>
-      {true && (
-        <div className='w-full h-[90vh] bg-[#F5F9FC] backdrop-blur-lg z-50 flex justify-center items-center'>
-          <div className={styles.loader} />
-        </div>
-      )}
-    </>
-  )
-}
+    <div className='w-full h-[90vh] bg-[#F5F9FC] backdrop-blur-lg z-50 flex justify-center items-center'>
+      <div className={styles.loader} />
+    </div>
+  );
+};
