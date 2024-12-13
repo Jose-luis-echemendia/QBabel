1ac20fa18cbca99d74a93ce1f121873fb64058af
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-13 11:13:37 -0500

    Update index.jsx

--- src/views/footer/index.jsx
+++ src/views/footer/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Link from 'next/link'
+
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 font-normal text-[12px] leading-[18px] font-poppins text-[#737791] mx-auto'>© 2024 VoyPati Made by <Link href='https://noxcreation.dev/' target='_blank' className='underline' rel='noreferrer'>NOX Creation</Link></div>
+    <>footer</>
   )
 }
 
