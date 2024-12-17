d7a61a4a623854f7bce2f44a750381f961f1c1d1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-17 00:32:49 -0500

    Update QBabel.jsx

--- src/QBabel.jsx
+++ src/QBabel.jsx
@@ -1,6 +1,11 @@
 import Routers from "./routers/routers-app";
 import "./styles/QBabel.css";
 
+import '@fontsource/anton';
+import '@fontsource/quicksand';
+import '@fontsource/open-sans';
+
+
 function QBabel() {
   return (
     <>
