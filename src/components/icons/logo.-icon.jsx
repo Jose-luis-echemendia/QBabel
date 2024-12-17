44c704ceae7cf09e46631b2d2dc76d34221d9508
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-17 00:32:46 -0500

    Create logo.-icon.jsx

---
+++ src/components/icons/logo.-icon.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+export const IconLogo = ({ alt, size, className }) => {
+    return (
+      <img
+        className={className}
+        src='/assets/Logoprincipal.svg'
+        alt={alt || 'App Logo'}
+        width={size || '40'} 
+        height={size || '40'}
+        style={{ objectFit: 'contain' }} 
+    />   
+
+    )
+};
+  
+  IconLogo.propTypes = {
+    alt: PropTypes.string,
+    size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+  };
+
+  
