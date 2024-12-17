732f60fef60a02c631fef9127427c906c4d90640
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-17 17:55:23 -0500

    Update index.jsx

--- src/views/home-page/index.jsx
+++ src/views/home-page/index.jsx
@@ -1,10 +1,31 @@
-
 const HomeView = () => {
   return (
-    <div className="h-96 w-96 bg-red-200">
-      <h1>HomePage</h1>
+    <div className="w-screen min-h-screen relative py-10 flex flex-row">
+      <img
+        src="/assets/Vector1.svg"
+        alt=""
+        className="absolute left-0 bottom-0"
+      />
+      <img
+        src="/assets/Vector2.svg"
+        alt=""
+        className="absolute right-0 bottom-0"
+      />
+      <div className="w-1/2">
+        <h1 className="">Bienvenidos a QBabel</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          impedit exercitationem inventore praesentium labore pariatur.
+          Molestiae corrupti quis odit aliquam, eveniet veritatis tempora ipsam,
+          fugiat nesciunt animi vel, rem in.
+        </p>
+      </div>
+      <div className="w-1/2 relative flex items-center justify-center">
+        <img src="/assets/image1.png" alt="" className="z-10"/>
+        <img src="/assets/image2.png" alt="" className=""/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeView
+export default HomeView;
