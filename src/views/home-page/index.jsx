656e1400d31ac21700f5e6f544b17902ecc07efe
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-17 00:32:51 -0500

    Update index.jsx

--- src/views/home-page/index.jsx
+++ src/views/home-page/index.jsx
@@ -1,9 +1,10 @@
 
-export const HomeView = () => {
+const HomeView = () => {
   return (
     <div className="h-96 w-96 bg-red-200">
       <h1>HomePage</h1>
-
     </div>
   )
 }
+
+export default HomeView
