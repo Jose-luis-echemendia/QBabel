9e11c5f74bdfc6532719d5ca47db756b1783cf1f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-17 00:32:58 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EAD38D',
+      
+        'white-100': '#FFFFFF',
+
+        'black-500': '#2E2E2E'
+      },
+
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], 
+        anton: ['Anton', 'sans-serif'], 
+        quicksand: ['Quicksand', 'sans-serif'], 
+        opensans: ['Open Sans', 'sans-serif'], 
+      },
+    },
   },
   plugins: [],
-}
-
+};
