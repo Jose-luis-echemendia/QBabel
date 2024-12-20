116a254d8010c79d0e5169e0a3e7681b1d5923c3
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-20 00:30:42 -0500

    Update get-auth-links.jsx

--- src/views/navbar/auth-links/get-auth-links.jsx
+++ src/views/navbar/auth-links/get-auth-links.jsx
@@ -3,14 +3,14 @@ import { CustomIcon } from "@/components/icons/custom-icons";
 export const GetAuthLinks = () => {
   return (
     <>
-      <div className="flex items-center gap-10">
+      <div className="flex items-center gap-10 mr-2">
         <div className="flex items-center gap-3">
           <a href=""><span>Registrarse</span></a>
-          <CustomIcon src="/assets/iconRegistrarse.svg" />
+          <CustomIcon src="/assets/icons/iconRegistrarse.svg" />
         </div>
         <div className="flex items-center gap-3">
           <a href=""><span>Iniciar sesión</span></a>
-          <CustomIcon src="/assets/iconIniciarSesion.svg" />
+          <CustomIcon src="/assets/icons/iconIniciarSesion.svg" />
         </div>
       </div>
     </>
