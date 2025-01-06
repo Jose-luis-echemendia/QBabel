17afa8af59a9079261d73a88997ea4c5ea33d1c6
Andy <andy18wrld@gmail.com>
2025-01-05 20:50:23 -0500

    Creado Componente 1

---
+++ src/views/home-page/explota-tu-talento/Componente1.jsx
@@ -0,0 +1,14 @@
+export const Componente1= ()=>{
+    return (
+        <div>
+
+        <img
+            src="public/assets/images/home/explota_talento/Vector (2).png" 
+            alt="" 
+            
+            />
+        
+        </div>
+
+    )
+}
