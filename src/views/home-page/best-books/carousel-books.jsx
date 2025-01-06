277de4a76f024a9296245a83e02ddbb2246d0f48
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-06 14:25:27 -0500

    Update carousel-books.jsx

--- src/views/home-page/best-books/carousel-books.jsx
+++ src/views/home-page/best-books/carousel-books.jsx
@@ -2,7 +2,7 @@ export const CarouselBooks = () => {
   return (
     <>
       <div className="flex flex-col gap-3">
-        <h6 className="font-opensans text-white">historias similares</h6>
+        <h6 className="font-opensans text-white mx-auto">historias similares</h6>
         <div className="flex flex-row gap-2">
           <div className="border-2 border-solid border-red-500 p-5" />
           <div className="border-2 border-solid border-red-500 p-5" />
