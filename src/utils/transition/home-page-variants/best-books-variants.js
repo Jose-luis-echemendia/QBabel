b5aa22ca75d83b012c1c3b38ae17b5241ad1e8c4
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-06 13:46:18 -0500

    Create best-books-variants.js

---
+++ src/utils/transition/home-page-variants/best-books-variants.js
@@ -0,0 +1,13 @@
+import { createContainerVariants, createChildrenVariants } from "@/utils/transition/MotionVariants";
+
+
+export const containerVariants = createContainerVariants({
+  staggerChildren: 0.5, // Ajusta el retraso entre los hijos
+  delayChildren: 0.2,   // Ajusta el retraso inicial para los hijos
+});
+
+export const childrenVariants = createChildrenVariants({
+  yOffset: 20,          // Desplazamiento vertical inicial
+  blurAmount: 4,        // Cantidad de desenfoque inicial
+  duration: 1.0,        // Duración de la animación
+});
