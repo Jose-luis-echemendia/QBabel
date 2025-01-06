1f15156fb7b3c1fa05a63a8737c0e32f1db441ff
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-06 14:12:25 -0500

    Update description-book.jsx

--- src/views/home-page/best-books/description-book.jsx
+++ src/views/home-page/best-books/description-book.jsx
@@ -1,6 +1,8 @@
-import { containerVariants, childrenVariants } from "@/utils/home-page-variants/best-books-variants";
+import {
+  containerVariants,
+  childrenVariants,
+} from "@/utils/home-page-variants/best-books-variants";
 import { motion } from "framer-motion";
-
 
 export const DescriptionBook = () => {
   return (
@@ -10,36 +12,35 @@ export const DescriptionBook = () => {
           variants={containerVariants}
           initial="hidden"
           whileInView="show"
-          className="text-white"
-          viewport={{once:true}}
+          className="text-white text-left flex flex-col"
+          viewport={{ once: true }}
         >
-          <motion.div
-            variants={childrenVariants}
-          >
-            <div>Author</div>
-            <div>Title</div>
+          <motion.div variants={childrenVariants}>
+            <span className="font-anton text-xl">Laura Sebastian</span>
+            <h2 className="font-anton text-6xl mt-5">Princesas de Cenizas</h2>
           </motion.div>
-          <motion.div
-            variants={childrenVariants}
-          >
-            <div className="mt-4">
+          <motion.p variants={childrenVariants}>
+            <div className="mt-4 mr-20 text-balance font-quicksand">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
               sequi, rem magnam nesciunt minima placeat, itaque eum neque
               officiis unde, eaque optio ratione aliquid assumenda facere ab et
-              quasi ducimus aut doloribus non numquam.
+              quasi ducimus aut doloribus non numquam. Lorem ipsum dolor, sit
+              amet consectetur adipisicing elit. Quisquam quidem perferendis ab
+              doloribus ipsam ratione fugit officia, rem necessitatibus,
+              repellat itaque optio velit quia a corrupti cum impedit voluptates
+              iure?
             </div>
-          </motion.div>
+          </motion.p>
           <motion.div
             variants={childrenVariants}
+            className="flex gap-5 mt-10 justify-center"
           >
-            <div className="flex gap-5 mt-5 justify-center">
-              <button className="bg-white text-black font-medium py-2 px-8 rounded-md flex-grow-0 max-w-[150px]">
-                SEE MORE
-              </button>
-              <button className="bg-transparent text-white border border-white py-2 px-8 rounded-md flex-grow-0 max-w-[150px]">
-                SUBSCRIBE
-              </button>
-            </div>
+            <button className="bg-white text-black py-2 px-8 rounded-md flex-grow-0 max-w-[150px] font-opensans font-bold">
+              SEE MORE
+            </button>
+            <button className="bg-transparent text-white border border-white py-2 px-8 rounded-md flex-grow-0 max-w-[150px] font-opensans font-bold">
+              LEER
+            </button>
           </motion.div>
         </motion.article>
       </div>
