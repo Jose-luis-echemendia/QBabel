82060d6819c6473d22612afa5a34532d84e29302
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-07 15:35:01 -0500

    Update useCarousel.js

--- src/hooks/useCarousel.js
+++ src/hooks/useCarousel.js
@@ -1,43 +1,43 @@
 import { useState } from "react"
-import { bestBooksData, initialBestBookData } from "@/constants/home-page/best-books";
+import { bestBooksbooksData, initialBestBookbooksData } from "@/constants/home-page/best-books";
 
 export const useCarousel = () => {
-    const [booksData, setBooksData] = useState(bestBooksData.slice(1));
+    const [booksbooksData, setBooksbooksData] = useState(bestBooksbooksData.slice(1));
     
-    const [transitionData, settransitionData] = useState(bestBooksData[0]);
+    const [transitionbooksData, setTransitionbooksData] = useState(bestBooksbooksData[0]);
 
-    const [currentBookData, setCurrentBookData] = useState({
-      data: initialBestBookData,
+    const [currentBookbooksData, setCurrentBookbooksData] = useState({
+      booksData: initialBestBookbooksData,
       index: 0,
     });
 
     const handlePrev = () => {
-        setBooksData((prevData) => [
-            transitionData ? transitionData : initialBestBookData,
-            ...prevData.slice(0, prevData.length - 1),
+        setBooksbooksData((prevbooksData) => [
+            transitionbooksData ? transitionbooksData : initialBestBookbooksData,
+            ...prevbooksData.slice(0, prevbooksData.length - 1),
         ]);
         
-        handleCurrentData({
-            data: transitionData ? transitionData : dataSlice[0],
-            index: dataSlice.findIndex(
-                (ele) => ele.img === data[data.length - 1].img
+        setTransitionbooksData({
+            booksData: transitionbooksData ? transitionbooksData : bestBooksbooksData[0],
+            index: bestBooksbooksData.findIndex(
+                (ele) => ele.img === booksData[booksData.length - 1].img
             ),
         });
         
-        handleTransitionData(data[data.length - 1]);
+        handleTransitionbooksData(booksData[booksData.length - 1]);
     }
 
     const handleNext = () => {
-        setBooksData((prev) => prev.slice(1));
-        handleCurrentData({
-            data: transitionData ? transitionData: initialBestBookData,
-            index: dataSlice.findIndex((ele) =>ele.img === data[0].img),
+        setBooksbooksData((prev) => prev.slice(1));
+        setTransitionbooksData({
+            booksData: transitionbooksData ? transitionbooksData: initialBestBookbooksData,
+            index: bestBooksbooksData.findIndex((ele) =>ele.img === booksData[0].img),
         });
-        handleTransitionData(data[0]);
+        handleTransitionbooksData(booksData[0]);
         setTimeout(() => {
-            setBooksData((newData) => [
-                ...newData,
-                transitionData ? transitionData: initialBestBookData,
+            setBooksbooksData((newbooksData) => [
+                ...newbooksData,
+                transitionbooksData ? transitionbooksData: initialBestBookbooksData,
             ])
         }, 5)
     }
