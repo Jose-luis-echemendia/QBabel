d80ce4e1a17ea0a20402b3f55bc848882ab86bb1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-06 19:06:20 -0500

    Update loading-suspense.jsx

--- src/components/load-suspense/loading-suspense.jsx
+++ src/components/load-suspense/loading-suspense.jsx
@@ -2,7 +2,7 @@ import "../../styles/components/LoadingSuspense.css"
 
 export const LoadingSuspense = () => {
   return (
-    <div className="w-full h-[90vh] bg-[#F5F9FC] backdrop-blur-lg z-50 flex justify-center items-center">
+    <div className="w-full min-h-screen bg-[#F5F9FC] backdrop-blur-lg z-50 flex justify-center items-center">
       <div className="loader" />
     </div>
   );
