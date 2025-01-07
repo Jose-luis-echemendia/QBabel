5a8ec23baf43e1f076bf89ec2931befa8279f90b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-07 15:52:36 -0500

    Update carousel-books.jsx

--- src/views/home-page/best-books/carousel-books.jsx
+++ src/views/home-page/best-books/carousel-books.jsx
@@ -1,15 +1,56 @@
-export const CarouselBooks = () => {
+import { motion } from "framer-motion";
+
+export const CarouselBooks = ({ booksData }) => {
   return (
     <>
-      <div className="flex flex-col gap-3">
-        <h6 className="font-opensans text-white mx-auto text-lg">historias similares</h6>
+      <div className="flex flex-col gap-6">
+        <h6 className="font-opensans text-white mx-auto text-lg">
+          historias similares
+        </h6>
         <div className="flex flex-row gap-2">
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
-          <div className="border-2 border-solid border-red-500 px-14 py-24" />
+          {booksData.map((bookData) => (
+            <motion.div
+              key={bookData.img}
+              className="relative h-52 min-w-[250px] rounded-2xl shadow-md md:h-80 md:min-w-[250px]"
+              layout
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                },
+              }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{ type: "spring", damping: 20, stiffness: 100 }}
+            >
+              <motion.img
+                layoutId={bookData.img}
+                alt="transition Image"
+                src={bookData.img}
+                className="absolute h-full w-full object-cover rounded-2xl brightness-75"
+              />
+
+              <motion.div>
+                <motion.div
+                  layout
+                  className="mb-2 h-[2px] w-3 rounded-full bg-white"
+                />
+                <motion.div
+                  className="text-xs text-[#D5D5D6]"
+                  layoutId={bookData.author}
+                >
+                  {bookData.author}
+                </motion.div>
+                <motion.h1
+                  className="text-xl leading-6 text-white"
+                  layoutId={bookData.title}
+                >
+                  {bookData.title}
+                </motion.h1>
+              </motion.div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </>
