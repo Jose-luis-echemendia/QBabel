4fe7bf6533d8986d106238759ebbf6d6c3cdb03a
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 12:55:07 -0500

    Update index.jsx

--- src/views/home-page/best-books/index.jsx
+++ src/views/home-page/best-books/index.jsx
@@ -3,6 +3,7 @@ import { DescriptionBook } from "./description-book";
 import { AnimatePresence, motion } from "framer-motion";
 import { useCarousel } from "@/hooks/useCarousel";
 import { BookCover } from "./book-cover";
+import { Background } from "./background";
 
 export const BestBooks = () => {
   const { booksData, transitionBookData, currentBookData } = useCarousel();
@@ -11,23 +12,26 @@ export const BestBooks = () => {
     <section className="relative grid grid-cols-10 gap-3 overflow-hidden pb-10">
       {/* Fondo animado */}
       <AnimatePresence mode="wait">
-        
-      </AnimatePresence>
-
-      {/* Contenido principal */}
-      <div className="relative z-10 col-span-4 py-10">
-        <BookCover
+        <Background
           transitionBookData={transitionBookData}
           currentBookData={currentBookData}
         />
-      </div>
 
-      <div className="relative z-10 col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14">
-        <DescriptionBook
-          bookData={transitionBookData ? transitionBookData : currentBookData}
-        />
-        <CarouselBooks booksData={booksData} />
-      </div>
+        {/* Contenido principal */}
+        <div className="relative z-10 col-span-4 py-10">
+          <BookCover
+            transitionBookData={transitionBookData}
+            currentBookData={currentBookData}
+          />
+        </div>
+
+        <div className="relative z-10 col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14">
+          <DescriptionBook
+            bookData={transitionBookData ? transitionBookData : currentBookData}
+          />
+          <CarouselBooks booksData={booksData} />
+        </div>
+      </AnimatePresence>
     </section>
   );
 };
