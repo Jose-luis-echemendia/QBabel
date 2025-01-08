a08a8ecd1a907b40b718d03de0411381b321565e
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 12:51:37 -0500

    f

--- src/views/home-page/best-books/index.jsx
+++ src/views/home-page/best-books/index.jsx
@@ -9,41 +9,40 @@ export const BestBooks = () => {
 
   return (
     <section className="relative grid grid-cols-10 gap-3 overflow-hidden pb-10">
+      {/* Fondo animado */}
       <AnimatePresence mode="wait">
-        {/* background */}
         <motion.div
-          key={transitionBookData.color} // Usa un valor único que cambia con currentBookData
+          key={transitionBookData.color}
           className="absolute top-0 left-0 w-full h-full z-0"
-          style={{ background: `linear-gradient(to right, #111217, ${transitionBookData.color})` }}
+          style={{
+            background: `linear-gradient(to right, #111217, ${transitionBookData.color})`,
+          }}
           variants={{
-            initial: { x: "-100%" },
-            animate: { x: "0%" },
-            exit: { x: "100%" },
+            initial: { opacity: 0 },
+            animate: { opacity: 1 },
+            exit: { opacity: 0 },
           }}
           initial="initial"
           animate="animate"
           exit="exit"
           transition={{ duration: 0.6, ease: "easeInOut" }}
         />
-
-        {/* Image book*/}
-        <div className="col-span-4 py-10 z-10 relative">
-          <BookCover
-            transitionBookData={transitionBookData}
-            currentBookData={currentBookData}
-          />
-        </div>
-
-        <div className="col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14 z-10 relative">
-          {/* Información book*/}
-          <DescriptionBook
-            bookData={transitionBookData ? transitionBookData : currentBookData}
-          />
-
-          {/* caruesol books */}
-          <CarouselBooks booksData={booksData} />
-        </div>
       </AnimatePresence>
+
+      {/* Contenido principal */}
+      <div className="relative z-10 col-span-4 py-10">
+        <BookCover
+          transitionBookData={transitionBookData}
+          currentBookData={currentBookData}
+        />
+      </div>
+
+      <div className="relative z-10 col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14">
+        <DescriptionBook
+          bookData={transitionBookData ? transitionBookData : currentBookData}
+        />
+        <CarouselBooks booksData={booksData} />
+      </div>
     </section>
   );
 };
