083a5da739c2c2275022869f30ab18f8145b86c1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 00:43:00 -0500

    Update best-books.js

--- src/constants/home-page/best-books.js
+++ src/constants/home-page/best-books.js
@@ -1,60 +1,68 @@
 export const bestBooksData = [
-    {
-      author: "Laura Sebastian",
-      tittle: "Princesas de Cenizas",
-      img: "/assets/images/home/best_books/book1.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Jennifer Lynn Barnes",
-      tittle: "La Jugada Final",
-      img: "/assets/images/home/best_books/book2.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Ariana Godoy",
-      tittle: "Sigue Mi Voz",
-      img: "/assets/images/home/best_books/book3.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Stephanie Garber",
-      tittle: "Caraval",
-      img: "/assets/images/home/best_books/book4.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Antoine De Saint-Exupéry",
-      tittle: "El Principito",
-      img: "/assets/images/home/best_books/book5.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "J.K Rowling",
-      tittle: "Harry Potter y la Pieda Filosofal",
-      img: "/assets/images/home/best_books/book6.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Eva Muñoz",
-      tittle: "Lascivia",
-      img: "/assets/images/home/best_books/book7.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-    {
-      author: "Jessica Rivas",
-      tittle: "El Rey Oscuro",
-      img: "/assets/images/home/best_books/book8.jpg",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
-    },
-  ];
-  
+  {
+    author: "Laura Sebastian",
+    tittle: "Princesas de Cenizas",
+    img: "/assets/images/home/best_books/book1.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#5C505A",
+  },
+  {
+    author: "Jennifer Lynn Barnes",
+    tittle: "La Jugada Final",
+    img: "/assets/images/home/best_books/book2.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#6F151D",
+  },
+  {
+    author: "Ariana Godoy",
+    tittle: "Sigue Mi Voz",
+    img: "/assets/images/home/best_books/book3.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#272450",
+  },
+  {
+    author: "Stephanie Garber",
+    tittle: "Caraval",
+    img: "/assets/images/home/best_books/book4.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#488899",
+  },
+  {
+    author: "Antoine De Saint-Exupéry",
+    tittle: "El Principito",
+    img: "/assets/images/home/best_books/book5.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#A5A5A6",
+  },
+  {
+    author: "J.K Rowling",
+    tittle: "Harry Potter y la Pieda Filosofal",
+    img: "/assets/images/home/best_books/book6.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#12284C",
+  },
+  {
+    author: "Eva Muñoz",
+    tittle: "Lascivia",
+    img: "/assets/images/home/best_books/book7.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#182139",
+  },
+  {
+    author: "Jessica Rivas",
+    tittle: "El Rey Oscuro",
+    img: "/assets/images/home/best_books/book8.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus, repellat itaque optio velitquia a corrupti cum impedit voluptates iure",
+    color: "#404145",
+  },
+];
+
 export const initialBestBookData = bestBooksData[0];
