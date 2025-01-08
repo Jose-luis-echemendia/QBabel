b6c0a8423e7e14096518969806ac213749be0e77
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 12:53:16 -0500

    Update index.jsx

--- src/views/home-page/best-books/index.jsx
+++ src/views/home-page/best-books/index.jsx
@@ -11,22 +11,7 @@ export const BestBooks = () => {
     <section className="relative grid grid-cols-10 gap-3 overflow-hidden pb-10">
       {/* Fondo animado */}
       <AnimatePresence mode="wait">
-        <motion.div
-          key={transitionBookData.color}
-          className="absolute top-0 left-0 w-full h-full z-0"
-          style={{
-            background: `linear-gradient(to right, #111217, ${transitionBookData.color})`,
-          }}
-          variants={{
-            initial: { opacity: 0 },
-            animate: { opacity: 1 },
-            exit: { opacity: 0 },
-          }}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{ duration: 0.6, ease: "easeInOut" }}
-        />
+        
       </AnimatePresence>
 
       {/* Contenido principal */}
