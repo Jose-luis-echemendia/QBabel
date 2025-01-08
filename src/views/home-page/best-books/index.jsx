f8c354c2ccf2b3da16cfa799ca3246027af727d1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 12:50:59 -0500

    f

--- src/views/home-page/best-books/index.jsx
+++ src/views/home-page/best-books/index.jsx
@@ -8,13 +8,13 @@ export const BestBooks = () => {
   const { booksData, transitionBookData, currentBookData } = useCarousel();
 
   return (
-    <section className="grid grid-cols-10 gap-3 overflow-hidden relative pb-10">
+    <section className="relative grid grid-cols-10 gap-3 overflow-hidden pb-10">
       <AnimatePresence mode="wait">
         {/* background */}
         <motion.div
-          key={currentBookData.color} // Usa un valor único que cambia con currentBookData
-          className="absolute top-0 left-0 w-full h-full z-0 bg-gradient-to-r from-[#111217]"
-          style={{ background: `linear-gradient(to right, #111217, ${currentBookData.color})` }}
+          key={transitionBookData.color} // Usa un valor único que cambia con currentBookData
+          className="absolute top-0 left-0 w-full h-full z-0"
+          style={{ background: `linear-gradient(to right, #111217, ${transitionBookData.color})` }}
           variants={{
             initial: { x: "-100%" },
             animate: { x: "0%" },
@@ -25,16 +25,16 @@ export const BestBooks = () => {
           exit="exit"
           transition={{ duration: 0.6, ease: "easeInOut" }}
         />
-        {/* Imagen book*/}
 
-        <div className="col-span-4 py-10">
+        {/* Image book*/}
+        <div className="col-span-4 py-10 z-10 relative">
           <BookCover
             transitionBookData={transitionBookData}
             currentBookData={currentBookData}
           />
         </div>
 
-        <div className="col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14">
+        <div className="col-span-6 p-4 pt-10 ml-10 flex flex-col gap-14 z-10 relative">
           {/* Información book*/}
           <DescriptionBook
             bookData={transitionBookData ? transitionBookData : currentBookData}
