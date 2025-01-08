bde49de87b6c1d1acab2a4be410ed2e9ffbcf47b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 13:01:05 -0500

    d

--- src/views/home-page/best-books/background.jsx
+++ src/views/home-page/best-books/background.jsx
@@ -1,23 +1,32 @@
 import { motion } from "framer-motion";
 
-export const Background = ({transitionBookData, currentBookData}) => {
+export const Background = ({ transitionBookData, currentBookData }) => {
   return (
     <>
+      {transitionBookData && (
+        <motion.div
+          key={currentBookData.data.color}
+          className="absolute top-0 left-0 w-full h-full z-0 bg-opacity-75"
+          style={{
+            background: `linear-gradient(to right, #111217, ${currentBookData.data.color})`,
+          }}
+          variants={{
+            initial: { opacity: 0 },
+            animate: { opacity: 1 },
+            exit: { opacity: 0 },
+          }}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ duration: 0.6, ease: "easeInOut" }}
+        />
+      )}
       <motion.div
-        key={transitionBookData.color}
-        className="absolute top-0 left-0 w-full h-full z-0"
+        key={transitionBookData.color + "transition"}
+        className="absolute top-0 left-0 w-full h-full z-0 bg-opacity-75"
         style={{
           background: `linear-gradient(to right, #111217, ${transitionBookData.color})`,
         }}
-        variants={{
-          initial: { opacity: 0 },
-          animate: { opacity: 1 },
-          exit: { opacity: 0 },
-        }}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ duration: 0.6, ease: "easeInOut" }}
       />
     </>
   );
