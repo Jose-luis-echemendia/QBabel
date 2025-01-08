1b8a2dc42b55a7b1869d8845856535744ee0b45c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 12:53:15 -0500

    Create background.jsx

---
+++ src/views/home-page/best-books/background.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+
+export const background = () => {
+  return (
+    <>
+      <motion.div
+        key={transitionBookData.color}
+        className="absolute top-0 left-0 w-full h-full z-0"
+        style={{
+          background: `linear-gradient(to right, #111217, ${transitionBookData.color})`,
+        }}
+        variants={{
+          initial: { opacity: 0 },
+          animate: { opacity: 1 },
+          exit: { opacity: 0 },
+        }}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+      />
+    </>
+  );
+};
