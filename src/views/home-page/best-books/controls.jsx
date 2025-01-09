8adb0bd32e9b2bfb5ab444eea90f2e4472f30808
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 23:00:06 -0500

    Update controls.jsx

--- src/views/home-page/best-books/controls.jsx
+++ src/views/home-page/best-books/controls.jsx
@@ -1,15 +1,16 @@
 import CustomButton from "@/components/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 export const Controls = () => {
   return (
     <>
       <div className="flex flex-row gap-5">
         <CustomButton
-          name="anterior"
+          iconCenter={ChevronLeftIcon}
           className="p-5 h-0 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
         />
         <CustomButton
-          name="siguiente"
+          iconCenter={ChevronRightIcon}
           className="p-5 h-0 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
         />
       </div>
