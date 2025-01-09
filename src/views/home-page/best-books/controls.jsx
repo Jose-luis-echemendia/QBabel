4083def8283ac971ed65d2b9f9d134fbf8ab13a1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 23:11:30 -0500

    Update controls.jsx

--- src/views/home-page/best-books/controls.jsx
+++ src/views/home-page/best-books/controls.jsx
@@ -1,18 +1,20 @@
 import CustomButton from "@/components/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { Progress } from "@material-tailwind/react";
 
 export const Controls = () => {
   return (
     <>
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-row gap-5 ml-6 -mt-6">
         <CustomButton
-          iconCenter={ChevronLeftIcon}
-          className="p-5 h-0 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
+          IconCenter={ChevronLeftIcon}
+          className="p-5 h-14 w-14 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
         />
         <CustomButton
-          iconCenter={ChevronRightIcon}
-          className="p-5 h-0 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
+          IconCenter={ChevronRightIcon}
+          className="p-5 h-14 w-14 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
         />
+        <Progress />
       </div>
     </>
   );
