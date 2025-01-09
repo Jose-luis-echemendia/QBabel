38f6f97dc66a604fa4c340ebbde2607bec579f99
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 23:32:50 -0500

    Update controls.jsx

--- src/views/home-page/best-books/controls.jsx
+++ src/views/home-page/best-books/controls.jsx
@@ -4,22 +4,20 @@ import { Progress } from "@material-tailwind/react";
 
 export const Controls = () => {
   return (
-    <div className="flex flex-col gap-5 ml-6 -mt-6">
+    <div className="flex flex-row gap-4 ml-6 -mt-5 items-center">
       {/* Botones */}
-      <div className="flex flex-row gap-5">
-        <CustomButton
-          IconCenter={ChevronLeftIcon}
-          className="p-5 h-14 w-14 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
-        />
-        <CustomButton
-          IconCenter={ChevronRightIcon}
-          className="p-5 h-14 w-14 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
-        />
-      </div>
+      <CustomButton
+        IconCenter={ChevronLeftIcon}
+        className="p-5 h-14 w-14 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
+      />
+      <CustomButton
+        IconCenter={ChevronRightIcon}
+        className="p-5 h-14 w-14 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
+      />
 
       {/* Barra de Progreso */}
-      <div className="w-full">
-        <Progress value={25} size="sm"/>
+      <div className="w-full ml-5">
+        <Progress value={1} size="sm" variant="gradient"/>
       </div>
     </div>
   );
