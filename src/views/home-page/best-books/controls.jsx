ed51434513405914bf9a9b48b95281fa248e1398
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-09 00:11:15 -0500

    Update controls.jsx

--- src/views/home-page/best-books/controls.jsx
+++ src/views/home-page/best-books/controls.jsx
@@ -1,7 +1,8 @@
 import CustomButton from "@/components/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { Progress } from "./Progress";
 
-export const Controls = () => {
+export const Controls = ({currentIndex, length }) => {
   return (
     <div className="flex flex-row gap-4 ml-6 -mt-5 items-center">
       {/* Botones */}
@@ -13,7 +14,7 @@ export const Controls = () => {
         IconCenter={ChevronRightIcon}
         className="p-5 h-14 w-14 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
       />
-      <Progress />
+      <Progress currentIndex={currentIndex} length={length} />
     </div>
   );
 };
