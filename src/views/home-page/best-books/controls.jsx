8504fcaa13e1f60b4c71c925005aebc461c87887
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-08 23:20:36 -0500

    Update controls.jsx

--- src/views/home-page/best-books/controls.jsx
+++ src/views/home-page/best-books/controls.jsx
@@ -4,8 +4,9 @@ import { Progress } from "@material-tailwind/react";
 
 export const Controls = () => {
   return (
-    <>
-      <div className="flex flex-row gap-5 ml-6 -mt-6">
+    <div className="flex flex-col gap-5 ml-6 -mt-6">
+      {/* Botones */}
+      <div className="flex flex-row gap-5">
         <CustomButton
           IconCenter={ChevronLeftIcon}
           className="p-5 h-14 w-14 rounded-full bg-transparent text-white border border-white/50 flex-grow-0  hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
@@ -14,8 +15,12 @@ export const Controls = () => {
           IconCenter={ChevronRightIcon}
           className="p-5 h-14 w-14 rounded-full bg-white/50 text-black  flex-grow-0 hover:shadow-xl hover:shadow-white/50 hover:translate-y-[-2px] transition-transform duration-300"
         />
-        <Progress  value={25} size="sm" variant="gradient" color="red"/>
       </div>
-    </>
+
+      {/* Barra de Progreso */}
+      <div className="w-full">
+        <Progress value={25} size="sm"/>
+      </div>
+    </div>
   );
 };
