4bdfc3617f5b4cc4f4796ec7d1795021ca2203d6
Andy <andy18wrld@gmail.com>
2025-01-10 22:10:40 -0500

    update-phone-card

--- src/views/home-page/explota-tu-talento/phone-card.jsx
+++ src/views/home-page/explota-tu-talento/phone-card.jsx
@@ -5,40 +5,46 @@ export const PhoneCard = ({
   title,
   description,
   span,
-  bg,
+  src,
   textColorSpan,
+  descriptionColor,
 }) => {
   return (
-    <article className="bg-transparent  relative pt-8">
-      <div
-        className={`absolute z-[80] top-12 h-[470px] rounded-lg left-5 w-[225px] ${bg}`}
-        aria-labelledby={`card-${number}`}
-      >
-        <header className="flex w-fit mx-auto mt-4">
-          <span
-            id={`card-${number}`}
-            className={`text-[40px] font-bold font-inter ${textColorSpan}`}
-          >
-            {number}
-          </span>
-          <h3 className="text-[32px] font-quicksand font-bold mt-[6px]">
-            {title}
-          </h3>
-        </header>
-
-        <p className="ml-6 mt-6 text-[16px] font-quicksand absolute font-bold">
-          {description}
-        </p>
-        <span className="font-quicksand bottom-10 font-extrabold ml-6 absolute mt-6">
-          {span}
-        </span>
-      </div>
-      <div>
+    <article className="relative   bg-transparent pt-8">
+      <div className="relative   h-[507px]">
+        {/* Imagen */}
         <img
-          src="/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (2).png"
+          src={src}
           alt={`Imagen representativa de ${title}`}
-          className="relative w-[486ox] h-[507px] z-[100]"
+          className="w-full relative z-[100] h-full object-cover rounded-lg"
         />
+
+        {/* Texto sobrepuesto */}
+        <div
+          className={`absolute top-0 left-0 w-full   z-[130]  h-full rounded-lg flex flex-col justify-center items-center p-4 `}
+          aria-labelledby={`card-${number}`}
+        >
+          <header className="flex mb-6">
+            <span
+              id={`card-${number}`}
+              className={`text-[45px] font-bold font-inter ${textColorSpan}`}
+            >
+              {number}
+            </span>
+            <h3 className="text-[36px] font-quicksand font-bold mt-2">
+              {title}
+            </h3>
+          </header>
+
+          <div className="  px-4">
+            <p
+              className={`text-[16px] font-quicksand font-semibold ${descriptionColor}`}
+            >
+              {description}
+            </p>
+            <span className=" font-inter  font-bold">{span}</span>
+          </div>
+        </div>
       </div>
     </article>
   );
