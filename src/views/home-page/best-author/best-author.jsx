801dbcbdeead9a258dd95f1cc7b46e16564729e8
Andy <andy18wrld@gmail.com>
2025-01-12 01:39:04 -0500

    create best-autor

---
+++ src/views/home-page/best-author/best-author.jsx
@@ -0,0 +1,32 @@
+// best-author.jsx
+import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
+
+export const BestAuthor = ({ images }) => {
+  if (!images || images.length === 0) {
+    return <div>No images available</div>;
+  }
+
+  return (
+    <div>
+      {images.map(image => (
+        <img
+          key={uuid()}
+          src={image.src}
+          alt={image.alt || ''}
+          className={`absolute  ${image.className || ''}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+BestAuthor.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string,
+      className: PropTypes.string,
+    })
+  ).isRequired,
+};
