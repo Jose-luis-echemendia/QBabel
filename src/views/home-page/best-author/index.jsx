40d34d67a5dda45ac1d6087e3e15b2afd065a1e9
Andy <andy18wrld@gmail.com>
2025-01-12 01:40:13 -0500

    update index,js bestAutorPage

--- src/views/home-page/best-author/index.jsx
+++ src/views/home-page/best-author/index.jsx
@@ -1,5 +1,19 @@
-export const BestAuthor = () => {
+// best-author-page.jsx
+import { BestAuthor } from './best-author';
+import { schemaImages } from './schema/schema-images';
+
+export const BestAuthorPage = () => {
   return (
-    <section className="relative min-h-screen z-0  p-10">BestAuthor</section>
+    <section className="relative min-h-screen z-0 p-10">
+      <p className="text-center text-[20px] font-opensans font-semibold mt-16 text-[#644844]">
+        Find out more about what we do for writers →
+      </p>
+
+      <h2 className="text-center font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
+        Escritores destacados
+      </h2>
+
+      <BestAuthor images={schemaImages} />
+    </section>
   );
 };
