90569f618512046b83766500bdd5d505dc09ee45
Andy <andy18wrld@gmail.com>
2025-01-12 01:38:20 -0500

    create schema-imagen-autor

---
+++ src/views/home-page/best-author/schema/schema-images.js
@@ -0,0 +1,33 @@
+// schema-images.js
+export const schemaImages = [
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (34).png',
+    alt: 'Isabel',
+    className: 'w-72 z-[2] top-20 ',
+  },
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (34).png',
+    alt: 'Erich',
+    className: 'w-72 z-500 ',
+  },
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (35).png',
+    alt: 'Manuel',
+    className: 'w-72 opacity-70',
+  },
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (37).png',
+    alt: 'GLoria Lopez',
+    className: 'w-72 opacity-70',
+  },
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (36).png',
+    alt: 'Mark',
+    className: 'w-72 opacity-70',
+  },
+  {
+    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (38).png',
+    alt: 'Aurora Perez',
+    className: 'w-72 left-0',
+  },
+];
