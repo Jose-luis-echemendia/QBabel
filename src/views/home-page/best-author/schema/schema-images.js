0dc9706b2669d7fafe06186c98985de5e6c8b2e4
Andy <andy18wrld@gmail.com>
2025-01-13 16:08:39 -0500

    update url

--- src/views/home-page/best-author/schema/schema-images.js
+++ src/views/home-page/best-author/schema/schema-images.js
@@ -1,32 +1,32 @@
 // schema-images.js
 export const schemaImages = [
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (34).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (34).webp',
     alt: 'Isabel',
-    className: 'w-72 z-[2] top-20 ',
+    className: 'w-72 z-[120]  top-0 ',
   },
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (34).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (33).webp',
     alt: 'Erich',
-    className: 'w-72 z-500 ',
+    className: 'w-72 top-36 -left-24 z-500 top-0 ',
   },
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (35).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (35).webp',
     alt: 'Manuel',
-    className: 'w-72 opacity-70',
+    className: 'w-72  opacity-70',
   },
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (37).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (37).webp',
     alt: 'GLoria Lopez',
     className: 'w-72 opacity-70',
   },
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (36).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (36).webp',
     alt: 'Mark',
     className: 'w-72 opacity-70',
   },
   {
-    src: '/assets/images/home/explota_talento/Publicación de Instagram de Motivación Diaria (38).png',
+    src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (38).webp',
     alt: 'Aurora Perez',
     className: 'w-72 left-0',
   },
