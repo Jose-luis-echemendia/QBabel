89a3acc49a3c208f61235a4b01a6470fcf917aa8
Andy <andy18wrld@gmail.com>
2025-01-13 16:08:28 -0500

    as

--- src/views/home-page/best-author/index.jsx
+++ src/views/home-page/best-author/index.jsx
@@ -4,14 +4,20 @@ import { schemaImages } from './schema/schema-images';
 
 export const BestAuthorPage = () => {
   return (
-    <section className="relative min-h-screen z-0 p-10">
-      <p className="text-center text-[20px] font-opensans font-semibold mt-16 text-[#644844]">
+    <section className="relative w-full min-h-screen z-10 ">
+      <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
         Find out more about what we do for writers →
       </p>
 
-      <h2 className="text-center font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
+      <h2 className="mx-auto w-fit font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
         Escritores destacados
       </h2>
+
+      <img
+        src="/assets/images/home/best_author/Vector.png"
+        alt=""
+        className="relative -top-32 right-10 w-96"
+      />
 
       <BestAuthor images={schemaImages} />
     </section>
