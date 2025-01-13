8758c8ae0639095b5ed0876a703488458f5ab562
Andy <andy18wrld@gmail.com>
2025-01-13 16:37:31 -0500

    modify schema

--- src/views/home-page/best-author/schema/schema-images.js
+++ src/views/home-page/best-author/schema/schema-images.js
@@ -3,31 +3,31 @@ export const schemaImages = [
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (34).webp',
     alt: 'Isabel',
-    className: 'w-72 z-[120]  top-0 ',
+    className: ' z-10 left-7  top-0 ',
   },
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (33).webp',
     alt: 'Erich',
-    className: 'w-72 top-36 -left-24 z-500 top-0 ',
+    className: ' top-36 z-20 -left-24 z-500 top-0 ',
   },
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (35).webp',
     alt: 'Manuel',
-    className: 'w-72  opacity-70',
+    className: ' top-64 left-8  opacity-70',
   },
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (37).webp',
     alt: 'GLoria Lopez',
-    className: 'w-72 opacity-70',
+    className: ' z-40 top-[370px] -left-16 left-0 opacity-70',
   },
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (36).webp',
     alt: 'Mark',
-    className: 'w-72 opacity-70',
+    className: ' bottom-[315px] left-24 opacity-70',
   },
   {
     src: '/assets/images/home/best_author/Publicación de Instagram de Motivación Diaria (38).webp',
     alt: 'Aurora Perez',
-    className: 'w-72 left-0',
+    className: '   z-50 bottom-48 -left-20',
   },
 ];
