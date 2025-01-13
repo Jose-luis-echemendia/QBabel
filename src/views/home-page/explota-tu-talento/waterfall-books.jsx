83fd80404072c49eec10439b2d93798ca5b816cb
Andy <andy18wrld@gmail.com>
2025-01-13 16:09:18 -0500

    create use memo

--- src/views/home-page/explota-tu-talento/waterfall-books.jsx
+++ src/views/home-page/explota-tu-talento/waterfall-books.jsx
@@ -1,12 +1,19 @@
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+
 import { v4 as uuid } from 'uuid';
 
 export const WaterfallBooks = ({ images }) => {
+  const imagesWithKeys = useMemo(
+    () => images.map(image => ({ ...image, key: uuid() })),
+    [images]
+  );
+
   return (
     <div>
-      {images.map(image => (
+      {imagesWithKeys.map(image => (
         <img
-          key={uuid()}
+          key={image.key}
           src={image.src}
           alt={image.alt || ''}
           className={`absolute ${image.className || ''}`}
