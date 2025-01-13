c2af3d873796c3dead9e49ebef5942f29b846910
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-01-13 00:08:12 -0500

    Update index.jsx

--- src/views/home-page/Opinions/index.jsx
+++ src/views/home-page/Opinions/index.jsx
@@ -1,11 +1,30 @@
-
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 export const Opinions = () => {
-  
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const pages = [
+      { id: 1, content: "Página 1 - Bienvenido al Libro" },
+      { id: 2, content: "Página 2 - Animaciones con Framer Motion" },
+      { id: 3, content: "Página 3 - ¡Gracias por leer!" },
+    ];
+    
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+          setCurrentPage((prev) => prev + 1);
+        }
+      };
+    
+      const handlePrevPage = () => {
+        if (currentPage > 0) {
+          setCurrentPage((prev) => prev - 1);
+        }
+      };
 
   return (
     <section className="relative min-h-screen flex items-center justify-center p-5 overflow-hidden pt-6 pb-4">
-     sdf
+     Opiniones
     </section>
   );
 };
