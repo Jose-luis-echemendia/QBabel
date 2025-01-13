f2cf097a31341ae3364607050dc4172d90b7993f
Andy <andy18wrld@gmail.com>
2025-01-13 16:37:19 -0500

    best-aauthor

--- src/views/home-page/best-author/best-author.jsx
+++ src/views/home-page/best-author/best-author.jsx
@@ -14,7 +14,9 @@ export const BestAuthor = ({ images }) => {
           key={uuid()}
           src={image.src}
           alt={image.alt || ''}
-          className={`absolute opacity-70  ${image.className || ''}`}
+          className={`absolute w-[300px] top-  opacity-70  ${
+            image.className || ''
+          }`}
         />
       ))}
     </div>
