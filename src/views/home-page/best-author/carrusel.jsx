68bdcc3b35598d10ac69bb495866b825a1f7e02c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-14 18:26:26 -0500

    Update carrusel.jsx

--- src/views/home-page/best-author/carrusel.jsx
+++ src/views/home-page/best-author/carrusel.jsx
@@ -1,10 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import ".styles.css"
 
 // import required modules
 import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
@@ -14,33 +15,36 @@ import { schemaCarrusel } from "./schema/schema-images";
 export const Carrusel = () => {
   return (
     <>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination, Autoplay]}
-        autoplay={{
-          delay: 1000, // Tiempo en milisegundos entre cada transición (por ejemplo, 5000 para 5 segundos)
-          disableOnInteraction: false, // Desactivar autoplay cuando el usuario interactúa (arrastra o hace clic)
-        }}
-        className="mySwiper"
-      >
-        {schemaCarrusel.map((image) => (
-        <SwiperSlide key={uuid()}>
-          <figure><img src={image.src} alt={image.alt}/></figure>
-        </SwiperSlide>
-
-        ))}
-      </Swiper>
+      <div className="-mt-10 h-full max-w-7xl">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          autoplay={{
+            delay: 5000, // Tiempo en milisegundos entre cada transición (por ejemplo, 5000 para 5 segundos)
+            disableOnInteraction: false, // Desactivar autoplay cuando el usuario interactúa (arrastra o hace clic)
+          }}
+          className="mySwiper"
+        >
+          {schemaCarrusel.map((image) => (
+            <SwiperSlide key={uuid()}>
+              <figure className="">
+                <img src={image.src} alt={image.alt} />
+              </figure>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };
