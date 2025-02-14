03c7bb0fa09a3dc2fba5c7bd328e507d7017223a
Andywrld <andy18wrld@gmail.com>
2025-02-14 18:24:56 -0500

    scrool

--- src/views/home-page/categoria/index.jsx
+++ src/views/home-page/categoria/index.jsx
@@ -1,17 +1,24 @@
-import { schemaImagesCategoria } from "./images-categoria"
-import { Card } from "./card"
-const Categoria = () => {
-  return (
-    <div>
-      <div className=" flex gap-4 w-full ">
-      {
-        schemaImagesCategoria.map((item,)=>(
-          <Card src={item.src} alt={item.alt} text={item.alt} />
-        ))
-      }
-      </div>
-    </div>
-  )
-}
+  import { schemaImagesCategoria } from "./images-categoria"
+  import { Card } from "./card"
+  import { motion } from "framer-motion"
+  const Categoria = () => {
+    return (
+      <div className="border border-gray-400 bg-gray-200 ">
+        <motion.div
+        initial={{ x: 0 }}
+        animate={{x:'-100%'}}
+        transition={{duration:26, repeat: Infinity, ease:"linear",}}
 
-export default Categoria;
+        
+        className=" flex gap-4 flex-nowrap min-w-max ">
+        {
+          schemaImagesCategoria.map((item,)=>(
+            <Card src={item.src} alt={item.alt} text={item.alt} />
+          ))
+        }
+        </motion.div>
+      </div>
+    )
+  }
+
+  export default Categoria;
