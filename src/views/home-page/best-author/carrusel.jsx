49b32d951655c5437122ca8b8c64516d66ae8567
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-14 18:40:25 -0500

    Update carrusel.jsx

--- src/views/home-page/best-author/carrusel.jsx
+++ src/views/home-page/best-author/carrusel.jsx
@@ -15,36 +15,34 @@ import { schemaCarrusel } from "./schema/schema-images";
 export const Carrusel = () => {
   return (
     <>
-      <div className=" h-full max-w-7xl">
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination, Autoplay]}
-          autoplay={{
-            delay: 5000, // Tiempo en milisegundos entre cada transición (por ejemplo, 5000 para 5 segundos)
-            disableOnInteraction: false, // Desactivar autoplay cuando el usuario interactúa (arrastra o hace clic)
-          }}
-          className="mySwiper"
-        >
-          {schemaCarrusel.map((image) => (
-            <SwiperSlide key={uuid()}>
-              <figure className="">
-                <img src={image.src} alt={image.alt} />
-              </figure>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+        autoplay={{
+          delay: 5000, // Tiempo en milisegundos entre cada transición (por ejemplo, 5000 para 5 segundos)
+          disableOnInteraction: false, // Desactivar autoplay cuando el usuario interactúa (arrastra o hace clic)
+        }}
+        className="mySwiper"
+      >
+        {schemaCarrusel.map((image) => (
+          <SwiperSlide key={uuid()}>
+            <figure className="">
+              <img src={image.src} alt={image.alt} />
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
