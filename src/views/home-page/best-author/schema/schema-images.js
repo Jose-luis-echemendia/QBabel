8005e7f8e87d8e0afe281c2dec2b3fe16075abef
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-14 18:57:05 -0500

    Update schema-images.js

--- src/views/home-page/best-author/schema/schema-images.js
+++ src/views/home-page/best-author/schema/schema-images.js
@@ -1,73 +1,100 @@
 // schema-images.js
 export const schemaImages = [
   {
-    src: '/assets/images/home/best_author/5.webp',
-    alt: 'Isabel',
-    className: ' z-10 left-7  top-0 ',
+    src: "/assets/images/home/best_author/5.webp",
+    alt: "Isabel",
+    className: " z-10 left-7  top-0 ",
   },
   {
-    src: '/assets/images/home/best_author/4.webp',
-    alt: 'Erich',
-    className: ' top-36 z-20 -left-24 z-500 top-0 ',
+    src: "/assets/images/home/best_author/4.webp",
+    alt: "Erich",
+    className: " top-36 z-20 -left-24 z-500 top-0 ",
   },
   {
-    src: '/assets/images/home/best_author/6.webp',
-    alt: 'Manuel',
-    className: ' top-64 left-8  opacity-70',
+    src: "/assets/images/home/best_author/6.webp",
+    alt: "Manuel",
+    className: " top-64 left-8  opacity-70",
   },
   {
-    src: '/assets/images/home/best_author/8.webp',
-    alt: 'GLoria Lopez',
-    className: ' z-40 top-[370px] -left-16 left-0 opacity-70',
+    src: "/assets/images/home/best_author/8.webp",
+    alt: "GLoria Lopez",
+    className: " z-40 top-[370px] -left-16 left-0 opacity-70",
   },
   {
-    src: '/assets/images/home/best_author/7.webp',
-    alt: 'Mark',
-    className: ' bottom-[315px] left-24 opacity-70',
+    src: "/assets/images/home/best_author/7.webp",
+    alt: "Mark",
+    className: " bottom-[315px] left-24 opacity-70",
   },
   {
-    src: '/assets/images/home/best_author/9.webp',
-    alt: 'Aurora Perez',
-    className: '   z-50 bottom-48 -left-20',
+    src: "/assets/images/home/best_author/9.webp",
+    alt: "Aurora Perez",
+    className: "   z-50 bottom-48 -left-20",
   },
 ];
 
 // Fotos contenida en el carrusel
 export const schemaCarrusel = [
   {
-    src: '/assets/images/home/best_author/1.webp',
-    alt: 'Ariana Godoy',
+    src: "/assets/images/home/best_author/1.webp",
+    alt: "Ariana Godoy",
+    color: "#9D5EC1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/2.webp',
-    alt: 'Lauren Roberts',
+    src: "/assets/images/home/best_author/2.webp",
+    alt: "Lauren Roberts",
+    color: "#130F44",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/3.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/3.webp",
+    alt: "Isabel",
+    color: "#E9DBAE",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/4.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/4.webp",
+    alt: "Isabel",
+    color: "#9e7fb0",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/5.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/5.webp",
+    alt: "Isabel",
+    color: "#2f2a69",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/6.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/6.webp",
+    alt: "Isabel",
+    color: "#e1d7b5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/7.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/7.webp",
+    alt: "Isabel",
+    color: "#9D5EC1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/8.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/8.webp",
+    alt: "Isabel",
+    color: "#514d82",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
   {
-    src: '/assets/images/home/best_author/9.webp',
-    alt: 'Isabel',
+    src: "/assets/images/home/best_author/9.webp",
+    alt: "Isabel",
+    color: "#dad4bf",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum non labore modi quae incidunt impedit aliquam, facere dolores similique dicta maxime et aut sapiente libero laudantium ea pariatur nisi soluta?",
   },
-]
+];
