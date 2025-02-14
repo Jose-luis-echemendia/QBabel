68a7695647fcfc08eb48815bdcca91b2554c96ca
Andywrld <andy18wrld@gmail.com>
2025-02-14 17:46:13 -0500

    todavia

---
+++ src/views/home-page/categoria/images-categoria.js
@@ -0,0 +1,86 @@
+export const schemaImagesCategoria = [
+    {
+      src: '/assets/images/home/categoria/Accion.png',
+      alt: 'Accion',
+    },
+    {
+      src: '/assets/images/home/categoria/Aventura.png',
+      alt: 'Aventura',
+    },
+   /* {
+      src: '/assets/images/home/categoria/Chick-it.png',
+      alt: 'Chick-it',
+    },
+    {
+      src: '/assets/images/home/categoria/Ciencia-Ficcion.png',
+      alt: 'Ciencia-Ficcion'
+    },
+    {
+      src: '/assets/images/home/categoria/Clasicos.png',
+      alt: 'Clasico',
+    },
+    {
+      src: '/assets/images/home/categoria/Cocina.png',
+      alt: 'Ocina',
+    },
+    {
+        src: '/assets/images/home/categoria/Espiritual.png',
+        alt: 'Espiritual',
+      },
+      {
+        src: '/assets/images/home/categoria/Fantasia.png',
+        alt: 'Fantasia',
+      },
+      {
+        src: '/assets/images/home/categoria/Fantic.png',
+        alt: 'Fanatic',
+      },
+      {
+        src: '/assets/images/home/categoria/Historia_Corta.png',
+        alt: 'Historia Corta'
+      },
+      {
+        src: '/assets/images/home/categoria/Historico.png',
+        alt: 'Historico',
+      },
+      {
+        src: '/assets/images/home/categoria/Humor.png',
+        alt: 'Humor',
+      },
+      {
+        src: '/assets/images/home/categoria/Infantiles.png',
+        alt: 'Infantiles',
+      },
+      {
+        src: '/assets/images/home/categoria/Juvenil.png',
+        alt: 'Juvenil ',
+      },
+      {
+        src: '/assets/images/home/categoria/Misterio.png',
+        alt: 'Misterio',
+      },
+      {
+        src: '/assets/images/home/categoria/Paranormal.png',
+        alt: 'Paranormal'
+      },
+      {
+        src: '/assets/images/home/categoria/Poesia.png',
+        alt: 'Poesia',
+      },
+      {
+        src: '/assets/images/home/categoria/Premiados.png',
+        alt: 'Premiados',
+      },
+      {
+        src: '/assets/images/home/categoria/Romance.png',
+        alt: 'Romance'
+      },
+      {
+        src: '/assets/images/home/categoria/Suspenso.png',
+        alt: 'Suspenso',
+      },
+      {
+        src: '/assets/images/home/categoria/Terror.png',
+        alt: 'Terros',
+      },*/
+  ];
