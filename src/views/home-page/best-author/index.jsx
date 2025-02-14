393ceb98c4ed74407fdb3cd1cae410bf74ac750e
Andywrld <andy18wrld@gmail.com>
2025-02-14 17:56:39 -0500

    update index best autor

--- src/views/home-page/best-author/index.jsx
+++ src/views/home-page/best-author/index.jsx
@@ -1,21 +1,12 @@
 // best-author-page.jsx
-<<<<<<< HEAD
-import { BestAuthor } from './best-author';
-import { schemaImages } from './schema/schema-images';
-
-export const BestAuthorPage = () => {
-  return (
-    <section className="relative w-full min-h-screen z-10 ">
-     
-     <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
-=======
 import { Carrusel } from "./carrusel";
+import { BestAuthor } from "./best-author";
+import { schemaImages } from "./schema/schema-images";
 
 export const BestAuthorPage = () => {
   return (
     <section className="relative  w-full min-h-screen z-10 ">
       <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
->>>>>>> develop
         Find out more about what we do for writers →
       </p>
 
@@ -23,10 +14,7 @@ export const BestAuthorPage = () => {
         Escritores destacados
       </h2>
 
-<<<<<<< HEAD
-=======
       <div className="bg-red-500 h-14 w-2 mx-auto"></div>
->>>>>>> develop
 
       <img
         src="/assets/images/home/best_author/Vector.png"
@@ -34,22 +22,13 @@ export const BestAuthorPage = () => {
         className="relative -top-32  w-[500px] h-[900px]"
       />
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <BestAuthor images={schemaImages} />
    
-    
-   
-  
-     
-     
-     
-=======
+
+
     <Carrusel/>
->>>>>>> develop
-=======
-      <Carrusel />
->>>>>>> develop
+
     </section>
   );
 };
