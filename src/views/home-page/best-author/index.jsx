94f40e3ffd0a705afcd1a900877f5326ef7ad1db
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-14 18:26:28 -0500

    Update index.jsx

--- src/views/home-page/best-author/index.jsx
+++ src/views/home-page/best-author/index.jsx
@@ -3,24 +3,25 @@ import { Carrusel } from "./carrusel";
 
 export const BestAuthorPage = () => {
   return (
-    <section className="relative  w-full min-h-screen z-10 ">
-      <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
-        Find out more about what we do for writers →
-      </p>
+    <section className="relative w-full min-h-screen flex flex-col">
+      <div>
+        <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
+          Find out more about what we do for writers →
+        </p>
 
-      <h2 className="mx-auto w-fit font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
-        Escritores destacados
-      </h2>
+        <h2 className="mx-auto w-fit font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
+          Escritores destacados
+        </h2>
+      </div>
 
-      <div className="bg-red-500 h-14 w-2 mx-auto"></div>
-
-      <img
-        src="/assets/images/home/best_author/Vector.png"
-        alt=""
-        className="relative -top-32  w-[500px] h-[900px]"
-      />
-
-      <Carrusel />
+      <div className="flex flex-row justify-center items-center">
+        <img
+          src="/assets/images/home/best_author/Vector.png"
+          alt=""
+          className="absolute top-36 -left-0 w-[500px] h-[900px] z-[-1]"
+        />
+        <Carrusel />
+      </div>
     </section>
   );
 };
