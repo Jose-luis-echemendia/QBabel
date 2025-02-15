e028a12617babaf99e690cb14001f6c96cd3d5e7
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-14 23:10:15 -0500

    ok

--- src/views/home-page/best-author/carrusel.jsx
+++ src/views/home-page/best-author/carrusel.jsx
@@ -37,9 +37,9 @@ export const Carrusel = () => {
       >
         {schemaCarrusel.map((author) => (
           <SwiperSlide key={uuid()}>
-            <figure className={`${author.color} p-4`} >
+            <figure className={` p-4`} >
               <img src={author.src} alt={author.alt} className="h-full w-full" />
-              <figcaption>{author.description}</figcaption>
+              
             </figure>
           </SwiperSlide>
         ))}
