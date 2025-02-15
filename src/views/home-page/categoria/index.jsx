79d4c313490eba52f0e1d4f0fe0a032eb1caa282
Andywrld <andy18wrld@gmail.com>
2025-02-14 22:05:45 -0500

    scroll

--- src/views/home-page/categoria/index.jsx
+++ src/views/home-page/categoria/index.jsx
@@ -1,30 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { schemaImagesCategoria } from "./images-categoria";
 import { Card } from "./card";
-import { motion } from "framer-motion";
+import { motion, useMotionValue, animate } from "framer-motion";
+import useMeasure from "react-use-measure";
 
 const Categoria = () => {
-  const [speed, setSpeed] = useState(30); // Velocidad normal
+  const FAST_SPEED = 40; 
+  const SLOW_SPEED = 100; 
+
+  const [duration, setDuration] = useState(FAST_SPEED); 
+  const [mustFinish, setMustFinish] = useState(false); 
+  const [rerender, setRerender] = useState(false); 
+
+  let [ref, { width }] = useMeasure(); 
+  const xTranslation = useMotionValue(0); 
+
+  useEffect(() => {
+    let controls;
+    let finalPosition = -width / 2 - 8; 
+
+    if (mustFinish) {
+      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+        ease: "linear",
+        duration: duration * (1 - xTranslation.get() / finalPosition),
+        onComplete: () => {
+          setMustFinish(false);
+          setRerender(!rerender);
+        },
+      });
+    } else {
+      controls = animate(xTranslation, [0, finalPosition], {
+        ease: "linear",
+        duration: duration,
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 0,
+      });
+    }
+
+    return () => controls?.stop?.();
+  }, [rerender, xTranslation, duration, width]);
 
   return (
     <div className="w-full overflow-hidden relative">
       <h1 className="text-center py-3 mt-10 text-[45px] font-quicksand font-bold">
-        Categoria 
+        Categoria
       </h1>
+
 
       <motion.div
         className="flex gap-6 flex-nowrap min-w-max hover:cursor-pointer"
-        initial={{ x: 0 }}
-        animate={{ x: ["0%", "-100%"] }} 
-        transition={{
-          ease: "linear",
-          duration: speed, 
-          repeat: Infinity,
+        style={{ x: xTranslation }}
+        ref={ref}
+        onHoverStart={() => {
+          setMustFinish(true);
+          setDuration(SLOW_SPEED); 
         }}
-        onHoverStart={() => setSpeed(50)} 
-        onHoverEnd={() => setSpeed(30)} 
+        onHoverEnd={() => {
+          setMustFinish(true);
+          setDuration(FAST_SPEED); 
+        }}
       >
-        {/* Duplicamos los elementos para que el scroll sea continuo */}
         {[...schemaImagesCategoria, ...schemaImagesCategoria].map((item, index) => (
           <Card key={index} src={item.src} alt={item.alt} text={item.alt} />
         ))}
