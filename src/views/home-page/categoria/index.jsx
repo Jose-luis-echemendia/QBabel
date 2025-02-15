77d548e7d357fe69010a8f3ebddfce8025bc6251
Andywrld <andy18wrld@gmail.com>
2025-02-14 19:17:01 -0500

    change scroll

--- src/views/home-page/categoria/index.jsx
+++ src/views/home-page/categoria/index.jsx
@@ -1,24 +1,67 @@
-  import { schemaImagesCategoria } from "./images-categoria"
-  import { Card } from "./card"
-  import { motion } from "framer-motion"
-  const Categoria = () => {
-    return (
-      <div className="border border-gray-400 bg-gray-200 ">
-        <motion.div
+import { useState } from "react";
+import { schemaImagesCategoria } from "./images-categoria";
+import { Card } from "./card";
+import { motion } from "framer-motion";
+
+const Categoria = () => {
+  const [speed, setSpeed] = useState(30); // Velocidad normal
+  const [mustFinish, setMustFinish] = useState(false);
+  const [rerender, setRerender] = useState(false);
+
+  useEffect(() => {
+    let controls;
+    let finalPosition = -width / 2 - 8;
+
+    if (mustFinish) {
+      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+        ease: "linear",
+        duration: duration * (1 - xTranslation.get() / finalPosition),
+        onComplete: () => {
+          setMustFinish(false);
+          setRerender(!rerender);
+        },
+      });
+    } else {
+      controls = animate(xTranslation, [0, finalPosition], {
+        ease: "linear",
+        duration: duration,
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 0,
+      });
+    }
+
+    return controls?.stop;
+  }, [rerender, xTranslation, duration, width]);
+
+  return (
+
+
+    <div className="w-full overflow-hidden relative">
+      <h1 className="text-center py-3 mt-10 text-[45px] font-quicksand font-bold">
+        Categoria 
+      </h1>
+
+      <motion.div
+        className="flex gap-6 flex-nowrap min-w-max hover:cursor-pointer"
         initial={{ x: 0 }}
-        animate={{x:'-100%'}}
-        transition={{duration:26, repeat: Infinity, ease:"linear",}}
-
+        animate={{ x: ["0%", "-100%"] }} // Se mueve completamente
+        transition={{
+          ease: "linear",
+          duration: speed, // Cambia la velocidad sin reiniciar
+          repeat: Infinity,
+        }}
+        onHoverStart={() => setSpeed(50)} // Hace el scroll más lento
+        onHoverEnd={() => setSpeed(30)} // Vuelve a la velocidad normal
         
-        className=" flex gap-4 flex-nowrap min-w-max ">
-        {
-          schemaImagesCategoria.map((item,)=>(
-            <Card src={item.src} alt={item.alt} text={item.alt} />
-          ))
-        }
-        </motion.div>
-      </div>
-    )
-  }
+      >
+        {/* Duplicamos los elementos para que el scroll sea continuo */}
+        {[...schemaImagesCategoria, ...schemaImagesCategoria].map((item, index) => (
+          <Card key={index} src={item.src} alt={item.alt} text={item.alt} />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
 
-  export default Categoria;
+export default Categoria;
