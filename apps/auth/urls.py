ad82ceff7f789ac4e080d70f81fa71b8215aad83
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-24 12:39:33 -0500

    Update urls.py

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BasicAuthView, AdminAuthView, LogoutView
+from .views import BasicAuthView, AdminAuthView, LogoutView, CustomTokenRefreshView
 
 urlpatterns = [
     path('jwt/users/', BasicAuthView.as_view(), name='basic-auth'),
     path('jwt/admins/', AdminAuthView.as_view(), name='admin-auth'),
+    path('api/token/refresh/custom/', CustomTokenRefreshView.as_view(), name='token_refresh_custom'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
     
