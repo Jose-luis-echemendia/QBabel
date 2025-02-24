d29c2043b483719794c7cda61e67e33d91d2ccdb
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-24 12:32:58 -0500

    Update urls.py

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BasicAuthView, AdminAuthView
+from .views import BasicAuthView, AdminAuthView, LogoutView
 
 urlpatterns = [
     path('jwt/users/', BasicAuthView.as_view(), name='basic-auth'),
     path('jwt/admins/', AdminAuthView.as_view(), name='admin-auth'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
     
