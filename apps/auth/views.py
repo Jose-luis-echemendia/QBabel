8dd1e06a2f6f4895c9d9fe55d0e613f1cb3eb1c5
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-24 12:32:57 -0500

    Update views.py

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,25 +1,34 @@
 from rest_framework.response import Response
+from django_ratelimit.decorators import ratelimit
+from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+from .serializers import LoginSerializer
 
 class BasicAuthView(APIView):
     permission_classes = [AllowAny]
 
+    @method_decorator(ratelimit(key='ip', rate='5/m', method='POST'))
     def post(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        password = request.data.get('password')
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({'error': 'Invalid input'}, status=400)
+
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
 
         user = authenticate(email=email, password=password)
-        if user:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'access': str(refresh.access_token),
-                'refresh': str(refresh),
-                'user_id': user.uid,
-            })
-        return Response({'error': 'Invalid credentials'}, status=401)
+        if not user:
+            return Response({'error': 'User not found or incorrect password'}, status=401)
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+            'user_id': user.uid,
+        })
 
 
 class AdminAuthView(APIView):
@@ -39,3 +48,12 @@ class AdminAuthView(APIView):
             })
         return Response({'error': 'Invalid credentials or not an admin'}, status=401)
 
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=204)
+        except Exception as e:
+            return Response(status=400)
