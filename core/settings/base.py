7afcef1bbcafa10ae6942fba3c0be73ba8339170
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-24 12:32:53 -0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -178,6 +178,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
+    'auth.backends.EmailBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
