331f89d6ef27c212132ff098236e1286b7088e81
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 08:34:02 -0500

    Update models.py

--- apps/category/models.py
+++ apps/category/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from apps.utils.models.abstract_models import BaseModel
+from apps.utils.models.models import GenericImage
 
-# Create your models here.
+class Category(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+    image = models.OneToOneField(GenericImage, on_delete=models.SET_NULL, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Category'
+        managed = True
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ("-created_at",)
