d7d1828a9a2ab480deb9ef94580b6eea9df61507
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 09:50:21 -0500

    Update serializers.py

--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -1,10 +1,7 @@
-from rest_framework import serializers
-from apps.user.models import UserAccount
 from apps.utils.serializers.abstract_serializers import AuditUserChangeSerializer, AbstractBaseSerializer, AbstractImageSerializer
-from apps.utils.serializers.serializers import GenericImage
 from .models import Category
 
-class CategorySerializer():
+class CategorySerializer(AbstractBaseSerializer, AuditUserChangeSerializer, AbstractImageSerializer):
     
     class Meta:
         model = Category
