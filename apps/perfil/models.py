3d2acf4185765ee941a8251be3c8aa4b48969a1b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 00:04:39 -0500

    Update models.py

--- apps/perfil/models.py
+++ apps/perfil/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.user.models import User
+from apps.utils.models.models import BaseModel
 
-# Create your models here.
+class Perfil(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        db_table = 'Perfil'
+        managed = True
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfils'
+        ordering = ("-created_at",)
