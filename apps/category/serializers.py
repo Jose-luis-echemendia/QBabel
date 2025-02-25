679076daa0c60846f3f08798172c0a9071540862
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 09:37:41 -0500

    Update serializers.py

--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -1,21 +1,15 @@
 from rest_framework import serializers
 from apps.user.models import UserAccount
-from apps.utils.serializers import AuditUserChangeSerializer, AbstractBaseSerializer
+from apps.utils.serializers import AuditUserChangeSerializer, AbstractBaseSerializer, AbstractImageSerializer
 from apps.utils.serializers import GenericImage, ImageSerializer
 
 class CategorySerializer():
-    image = serializers.PrimaryKeyRelatedField(
-        queryset=GenericImage.objects.all(), write_only=True
-    )
+    
     class Meta:
         fields = AbstractBaseSerializer.Meta + [
             "name",
-            "image",
-            "image_details"
             "description",
             "parent"            
-            ] + AuditUserChangeSerializer.Meta
+            ] + AuditUserChangeSerializer.Meta + AbstractImageSerializer.Meta
         
-    def get_image_details(self, obj):
-        return ImageSerializer(obj.image).data if obj.image else None
     
