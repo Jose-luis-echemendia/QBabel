bec2c4b92c5d9ee8f29051806522d895ddd1ec86
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 00:04:44 -0500

    Create signals.py

---
+++ apps/user/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from apps.perfil.models import Perfil
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def post_save_user_create_create_perfil(sender, instance, created,*args, **kwargs):
+    if created:
+        perfil_for_user = Perfil.objects.create(user=instance)
+        perfil_for_user.save()
