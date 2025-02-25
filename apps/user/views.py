ad45a57c7f87b1f1ecc7408d02e53aaa1851e8de
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 09:23:42 -0500

    Update views.py

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,7 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer
 
