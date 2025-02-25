e573732c5ade3c4a4e6cb086cdad218cba1eee08
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-25 00:19:45 -0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
+    path('api/', include('apps.user.urls')),
     path('api/', include('apps.authentication.urls')),
     
 ]
