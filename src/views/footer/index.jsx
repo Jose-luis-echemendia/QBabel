812cf827bb2cf2c6f5fd7e3dc3a3ff64df14d13e
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-26 14:40:55 -0500

    Update index.jsx

--- src/views/footer/index.jsx
+++ src/views/footer/index.jsx
@@ -1,10 +1,20 @@
+import { Decorator } from "./decorator";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-10 relative flex justify-center items-center bg-[#2E2E2E] text-primary">
-      <section>
-        <h2>QBabel siempre disponible para usted</h2>
+    <footer className="w-full max-h-screen h-full p-10 relative flex justify-center items-center bg-[#2E2E2E] text-primary">
+      <section className="flex flex-col justify-center items-center">
+        <h2 className="font-italianno mx-auto w-fit mt-2 font-bold text-[64px]">QBabel siempre disponible para usted</h2>
+        <div className="flex flex-row gap-20 justify-center items-center">
+          <h5 className="font-italianno mx-auto w-fit mt-2 font-bold text-[64px]">Has realidad tu historia!</h5>
+          <div>
+            <div className="">a</div>
+            <div className="">a</div>
+            <div className="">a</div>
+          </div>
+        </div>
       </section>
+      <Decorator/>
     </footer>
   )
 }
