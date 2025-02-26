5e8940ef6ca8c15efed4a7252009b98fc80fc86e
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-26 14:27:07 -0500

    Update index.jsx

--- src/views/footer/index.jsx
+++ src/views/footer/index.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className="w-full p-10 flex justify-center items-center bg-[#2E2E2E] text-primary">
+    <footer className="w-full p-10 relative flex justify-center items-center bg-[#2E2E2E] text-primary">
       <section>
         <h2>QBabel siempre disponible para usted</h2>
       </section>
