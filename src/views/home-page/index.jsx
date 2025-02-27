86f6e2fca0acf05f2fd161f78a7f6a557307bc89
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-27 00:25:04 -0500

    ok

--- src/views/home-page/index.jsx
+++ src/views/home-page/index.jsx
@@ -3,7 +3,7 @@ import { BestBooks } from './best-books';
 import { ExploitYourTalent } from './exploit-your-talent';
 import { Hero } from './hero';
 import { Opinions } from './Opinions';
-import Categoria from './categoria';
+import Categoria from './categories';
 
 const HomeView = () => {
   return (
