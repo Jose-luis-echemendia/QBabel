d4187cbf3d45b92fd6326e32d3b8d252dd06a82d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-27 18:46:54 -0500

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,16 +1,25 @@
-
 export const CustomFooter = () => {
   return (
-    <>  
+    <>
       <div className="flex gap-10">
-        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">Condiciones</a>
-        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">Privacidad</a>
-        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">Políticas de pagos</a>
-        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">Accesibilidad</a>
-        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">Ayuda</a>
-        <div className="h-4 w-[2px] bg-primary"/>
+        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">
+          Condiciones
+        </a>
+        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">
+          Privacidad
+        </a>
+        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">
+          Políticas de pagos
+        </a>
+        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">
+          Accesibilidad
+        </a>
+        <a className="text-white-100 hover:text-opacity-60 hover:cursor-pointer">
+          Ayuda
+        </a>
+        <div className="h-4 w-[2px] bg-primary" />
         <span className="text-white-100">© 2025 QBabel</span>
       </div>
     </>
-  )
-}
+  );
+};
