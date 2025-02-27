648b824da86a3501f68fd2416b10602b7b1f81b0
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-27 16:08:15 -0500

    Update get-auth-links.jsx

--- src/views/navbar/auth-links/get-auth-links.jsx
+++ src/views/navbar/auth-links/get-auth-links.jsx
@@ -1,36 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { CustomIcon } from "@/components/icons/custom-icons";
-import {CustomModal}  from "@/components/modal/index"; // Ajusta la ruta según tu estructura
+import { CustomModal } from "@/components/modal/index";
+import { Login } from "@/views/auth/Login";
+import { Signup } from "@/views/auth/Signup";
+
 
 export const GetAuthLinks = () => {
   // Estados para controlar la apertura y cierre de los modales
   const [openRegisterModal, setOpenRegisterModal] = useState(false);
   const [openLoginModal, setOpenLoginModal] = useState(false);
-
-  // Contenido del modal de registro
-  const registerModalContent = (
-    <div>
-      <h2>Registrarse</h2>
-      <form>
-        <input type="text" placeholder="Nombre" />
-        <input type="email" placeholder="Correo electrónico" />
-        <input type="password" placeholder="Contraseña" />
-        <button type="submit">Registrarse</button>
-      </form>
-    </div>
-  );
-
-  // Contenido del modal de inicio de sesión
-  const loginModalContent = (
-    <div>
-      <h2>Iniciar sesión</h2>
-      <form>
-        <input type="email" placeholder="Correo electrónico" />
-        <input type="password" placeholder="Contraseña" />
-        <button type="submit">Iniciar sesión</button>
-      </form>
-    </div>
-  );
 
   return (
     <>
@@ -60,7 +38,7 @@ export const GetAuthLinks = () => {
         classNameDialog="custom-dialog-class" // Clases personalizadas
         classNameBody="custom-body-class"
       >
-        {registerModalContent}
+        <Signup/>
       </CustomModal>
 
       {/* Modal de inicio de sesión */}
@@ -71,7 +49,7 @@ export const GetAuthLinks = () => {
         classNameDialog="custom-dialog-class" // Clases personalizadas
         classNameBody="custom-body-class"
       >
-        {loginModalContent}
+        <Login/>
       </CustomModal>
     </>
   );
