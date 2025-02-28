b129545653b53d59a6b9e90a5b895e80c4d624a9
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-28 00:43:09 -0500

    Update Login.jsx

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -51,7 +51,7 @@ export const Login = () => {
                     value={email}
                     autoComplete="email"
                     required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
                   />
                 </div>
               </div>
@@ -72,14 +72,14 @@ export const Login = () => {
                     value={password}
                     autoComplete="current-password"
                     required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
                   />
                 </div>
               </div>
 
               <div>
                 {!stateAuth ? (
-                  <button   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-[#2E2E2E] bg-primary hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <button   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-[#2E2E2E] bg-primary hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:border-primary">
                     <Oval
                       visible={true}
                       height="20"
@@ -93,7 +93,7 @@ export const Login = () => {
                 ) : (
                   <button
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-[#2E2E2E] bg-primary hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-medium text-[#2E2E2E] bg-primary hover:bg-primary-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#242424] focus:border-[#242424]"
                   >
                     Login
                   </button>
