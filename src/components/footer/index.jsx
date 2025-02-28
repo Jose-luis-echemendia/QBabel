af326f2e60d1a9d991764d1ee0e519d833bbe772
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-28 00:45:53 -0500

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -89,7 +89,7 @@ export const CustomFooter = () => {
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-400 hover:text-gray-500"
+              className="text-white-100 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
