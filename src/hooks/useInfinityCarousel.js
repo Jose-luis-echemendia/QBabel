12ec27b87c7efcc41c3258efca6ea8a6e4a488cf
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-27 23:37:56 -0500

    Update useInfinityCarousel.js

--- src/hooks/useInfinityCarousel.js
+++ src/hooks/useInfinityCarousel.js
@@ -7,52 +7,29 @@ export const useInfinityCarousel = ({
   slowSpeed = 160,
   direction = "left", // "left" o "right"
 }) => {
-  const [duration, setDuration] = useState(fastSpeed);
-  const [mustFinish, setMustFinish] = useState(false);
-  const [rerender, setRerender] = useState(false);
-
+  const [speed, setSpeed] = useState(fastSpeed);
   const [ref, { width }] = useMeasure();
   const xTranslation = useMotionValue(0);
 
   useEffect(() => {
-    let controls;
-    const finalPosition =
-      direction === "left" ? -width / 2 - 8 : width / 2 + 8; // Posición final según la dirección
+    if (width === 0) return;
 
-    if (mustFinish) {
-      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
-        ease: "linear",
-        duration: duration * (1 - xTranslation.get() / finalPosition),
-        onComplete: () => {
-          setMustFinish(false);
-          setRerender(!rerender);
-        },
-      });
-    } else {
-      const startPosition = direction === "left" ? 0 : -finalPosition;
-      const endPosition = direction === "left" ? finalPosition : 0;
+    const startPosition = direction === "left" ? 0 : -width / 2;
+    const endPosition = direction === "left" ? -width / 2 : 0;
 
-      controls = animate(xTranslation, [startPosition, endPosition], {
-        ease: "linear",
-        duration: duration,
-        repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 0,
-      });
-    }
+    const controls = animate(xTranslation, [startPosition, endPosition], {
+      ease: "linear",
+      duration: speed, // Controla la velocidad de desplazamiento
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
 
-    return () => controls?.stop?.();
-  }, [rerender, xTranslation, duration, width, direction]);
+    return () => controls.stop();
+  }, [speed, width, direction]);
 
-  const handleHoverStart = () => {
-    setMustFinish(true);
-    setDuration(slowSpeed);
-  };
-
-  const handleHoverEnd = () => {
-    setMustFinish(true);
-    setDuration(fastSpeed);
-  };
+  const handleHoverStart = () => setSpeed(slowSpeed);
+  const handleHoverEnd = () => setSpeed(fastSpeed);
 
   return {
     ref,
