c1850cb2123206b01b9f3a1f30aae036ab2e847a
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 14:33:27 -0500

    Update slice.js

--- src/store/auth/slice.js
+++ src/store/auth/slice.js
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = []
+const initialState = [{
+    isAuthenticated: false,
+    jwtTokenAccess: null,
+    jwtTokenRefresh: null,
+    user:{}
+}]
 
 export const authSlice = createSlice({
     name : 'auth',
