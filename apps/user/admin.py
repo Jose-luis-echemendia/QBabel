2c5ec2a3fce9fb6b4070dc9840c7f51a069af2ac
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-28 22:50:45 -0500

    Update admin.py

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserAccount
 
-# Register your models here.
+class UserAccountAdmin(UserAdmin):
+    list_display = ('email', 'user_name', 'is_staff', 'is_active')
+    search_fields = ('email', 'user_name')
+    ordering = ('email',)
+
+admin.site.register(UserAccount, UserAccountAdmin)
