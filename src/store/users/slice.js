245cb603d338f895bb4cec31365f94f34991ad1f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 14:31:17 -0500

    Create slice.js

---
+++ src/store/users/slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = []
+
+export const usersSlice = createSlice({
+    name : 'users',
+    initialState: initialState,
+    reducers: {
+
+    }
+})
+
+export default usersSlice.reducer
