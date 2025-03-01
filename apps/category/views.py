3fe90a0f98f9932bc318bd3b2a493f2f4fe09231
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-28 22:23:05 -0500

    Update views.py

--- apps/category/views.py
+++ apps/category/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Category
 from .serializers import CategorySerializer
 
-class CustomCategoryViewSet:
+class CustomCategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny, IsAuthenticated]
