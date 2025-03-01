e5e5c13b42bb5161c0ba9e3db7c20920c1d02a6c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-02-28 23:18:40 -0500

    Update urls.py

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('token/jwt/create/users/', BasicAuthView.as_view(), name='basic-auth'),
     path('token/jwt/create/admins/', AdminAuthView.as_view(), name='admin-auth'),
     path('token/jwt/refresh/custom/', CustomTokenRefreshView.as_view(), name='token_refresh_custom'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/custom/', LogoutView.as_view(), name='logout'),
 ]
     
