692a58f76f910e71bc3844fb15d6d0923b5585cc
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 14:31:12 -0500

    Create slice.js

---
+++ src/store/auth/slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = []
+
+export const authSlice = createSlice({
+    name : 'auth',
+    initialState: initialState,
+    reducers: {
+
+    }
+})
+
+export default authSlice.reducer
