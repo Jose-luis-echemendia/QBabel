dd9307b4a57341c0d333e2570658adccc48dacc5
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 14:56:47 -0500

    Create index.jsx

---
+++ src/views/home-page/index.jsx
@@ -0,0 +1,15 @@
+
+const HomeView = () => {
+  return (
+    <>
+      <div className="min-h-screen overflow-hidden">
+        <h2 id="footer-heading" className="sr-only">
+          Home
+        </h2>
+        <h2>sdfdf</h2>
+      </div>
+    </>
+  );
+};
+
+export default HomeView;
