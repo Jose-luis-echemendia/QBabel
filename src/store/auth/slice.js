0d84f512e1a2b2f4f7fb580cc0852531b115fe73
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 15:44:03 -0500

    ok

--- src/store/auth/slice.js
+++ src/store/auth/slice.js
@@ -1,18 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isAuthenticated: true,
-    jwtTokenAccess: null,
-    jwtTokenRefresh: null,
-    user:{}
-}
+  isAuthenticated: true,
+  jwtTokenAccess: null,
+  jwtTokenRefresh: null,
+  user: {},
+};
 
 export const authSlice = createSlice({
-    name : 'auth',
-    initialState: initialState,
-    reducers: {
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    login: (state, action) => {
+      console.log(state);
+      console.log(action);
+    },
+  },
+});
 
-    }
-})
-
-export default authSlice.reducer
+export default authSlice.reducer;
+export const { login } = authSlice.actions;
