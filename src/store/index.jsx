4f3c1709a60a13a797a4bda2166da8b4b758b540
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 14:31:14 -0500

    Update index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice"
+import authReducer from "./auth/slice"
 
 
 export const sotre = configureStore({
     reducer : {
-        users: usersReducer
+        auth: authReducer,
+        users: usersReducer,
     }
 })
