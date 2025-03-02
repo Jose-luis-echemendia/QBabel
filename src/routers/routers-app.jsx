22b1a6df9a53d3ca811087089b97b50e75fd7928
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-02 00:06:45 -0500

    Update routers-app.jsx

--- src/routers/routers-app.jsx
+++ src/routers/routers-app.jsx
@@ -1,29 +1,49 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "@/components/protected-route";
+import { useAppSelector } from "@/hooks/redux/useStore";
 
 //PAGES
 import HomePage from "@/pages/home/page";
-import WelcomePage from "@/pages/welcome-page/page"
+import WelcomePage from "@/pages/welcome-page/page";
 import { Error404 } from "@/pages/error/Error404";
 
 const Routers = () => {
+  const auth = useAppSelector((state) => state.auth);
   return (
     <>
       <Router>
         <Routes>
           {/* ERROR DISPLAY */}
-          <Route path="*" element={<Error404 />}></Route> 
-          
+          <Route path="*" element={<Error404 />}></Route>
 
           {/* HOME DISPLAY */}
-          <Route path="/" element={<WelcomePage />}></Route>
-          <Route path="/home" element={<HomePage />}></Route>
+          <Route
+            element={
+              <ProtectedRoute
+                redirectTo="/"
+                isAllowed={!!auth.userId && auth.isAuthenticated}
+              />
+            }
+          >
+            <Route path="/home" element={<HomePage />}></Route>
+          </Route>
+
+          <Route
+            element={
+              <ProtectedRoute
+                redirectTo="/home"
+                isAllowed={(auth.userId === undefined || auth.userId === null) && !auth.isAuthenticated}
+              />
+            }
+          >
+            <Route path="/" element={<WelcomePage />}></Route>
+          </Route>
 
           {/* PAGES DISPLAY */}
-
         </Routes>
       </Router>
     </>
   );
 };
 
-export default Routers
+export default Routers;
