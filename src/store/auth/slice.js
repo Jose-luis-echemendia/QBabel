443827297b185fc7afc37f9f5c4929cda03f3c40
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 23:22:09 -0500

    Update slice.js

--- src/store/auth/slice.js
+++ src/store/auth/slice.js
@@ -1,21 +1,60 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { loginThunk, logoutThunk, refreshTokenThunk } from "./thunks";
 
 const initialState = {
   isAuthenticated: true,
   jwtTokenAccess: null,
   jwtTokenRefresh: null,
   user: {},
-  loading: false
+  loading: false,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
-    login: (state, action) => {
-      console.log("state", state);
-      console.log("action", action);
+    logoutLocal: (state) => {
+      state.isAuthenticated = false;
+      state.jwtTokenAccess = null;
+      state.jwtTokenRefresh = null;
+      state.user = null;
+      localStorage.removeItem("jwtTokenAccess");
+      localStorage.removeItem("jwtTokenRefresh");
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      // **Login Reducers**
+      .addCase(loginThunk.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = true;
+        state.jwtTokenAccess = action.payload.accessToken;
+        state.jwtTokenRefresh = action.payload.refreshToken;
+        localStorage.setItem("jwtTokenAccess", action.payload.accessToken);
+        localStorage.setItem("jwtTokenRefresh", action.payload.refreshToken);
+      })
+      .addCase(loginThunk.rejected, (state) => {
+        state.loading = false;
+      })
+
+      // **Refresh Token Reducers**
+      .addCase(refreshTokenThunk.fulfilled, (state, action) => {
+        state.jwtTokenAccess = action.payload;
+        localStorage.setItem("jwtTokenAccess", action.payload);
+      })
+
+      // **Logout Reducers**
+      .addCase(logoutThunk.fulfilled, (state) => {
+        state.isAuthenticated = false;
+        state.jwtTokenAccess = null;
+        state.jwtTokenRefresh = null;
+        state.user = null;
+        localStorage.removeItem("jwtTokenAccess");
+        localStorage.removeItem("jwtTokenRefresh");
+      });
   },
 });
 
