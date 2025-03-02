9ed0ddbcd10071c12205596f982c1d3d9c8d5b62
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-01 23:27:13 -0500

    Update useAuth.js

--- src/hooks/redux/useAuth.js
+++ src/hooks/redux/useAuth.js
@@ -1,11 +1,11 @@
-import { login } from "@/store/auth/slice";
+import { loginThunk } from "@/store/auth/thunks";
 import { useAppDispatch } from "./useStore";
 
 export const useAuth = () => {
   const dispath = useAppDispatch();
 
   const handleLogin = ( email, password ) => {
-    dispath(login({ email, password }));
+    dispath(loginThunk({ email, password }));
   };
 
   return { handleLogin };
