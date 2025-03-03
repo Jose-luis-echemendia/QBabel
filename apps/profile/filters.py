19ab959d998e2d2772517871828b91fed58bef07
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-02 21:30:03 -0500

    Update filters.py

--- apps/profile/filters.py
+++ apps/profile/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import Profile
 
-class EmoteFilter(django_filters.FilterSet):
+class ProfileFilter(django_filters.FilterSet):
     me = django_filters.BooleanFilter(method='filter_me')
     created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
 
@@ -11,5 +11,5 @@ class EmoteFilter(django_filters.FilterSet):
 
     def filter_me(self, queryset, name, value):
         if value:
-            return queryset.filter(gallery__user=self.request.user)
+            return queryset.filter(user=self.request.user)
         return queryset
