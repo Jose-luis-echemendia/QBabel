fc7015a0084da69d7fcf823e2bb43c94acb79ad9
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-02 21:30:06 -0500

    Update views.py

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -1,34 +1,29 @@
-from apps.user.decorator import limit_daily_spending
-from apps.utils.helpers import CalculateSpent
-from apps.gallery.models import Gallery
-from apps.services.managers.image_generator import ImageGeneratorManager
-from .serializers import EmoteSerializer
-from .models import Emote
-from .filters import EmoteFilter
-from apps.utils.enums import GeneratedImageQualityTypes
+from .serializers import ProfileSerializer
+from .models import Profile
+from .filters import ProfileFilter
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db import transaction
 from apps.utils.pagination import MediumSetPagination
+from django.db import transaction
 
                
 class EmoteView(APIView):
     """
     view to handle emotes requested by the user
     """
-    serializer_class = EmoteSerializer
+    serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
-    filterset_class = EmoteFilter
+    filterset_class = ProfileFilter
     ordering_fields = ["created_at", "quality", "count_download", "count_images"]  
     ordering = ["created_at"]
 
     def get_queryset(self):
-        queryset = Emote.objects.all()
+        queryset = Profile.objects.all()
         if not self.request.user.is_superuser:
             queryset = queryset.filter(gallery__user=self.request.user)
         if "state" not in self.request.GET:
@@ -68,75 +63,8 @@ class EmoteView(APIView):
         emotes_data = self.serializer_class(results, many=True).data
         return paginator.get_paginated_response({"emotes": emotes_data})
             
-    @limit_daily_spending()
-    def post(self, request, *args, **kwargs):
-        """
-        Endpoint to generate the user's emote.
-        """
-        request_data = request.data.copy()
-
-        try:
-            # Verificar campo 'quality'
-            if not request_data.get("quality", False):
-                return Response(
-                    {"details": "quality field is required"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            if request_data.get("quality") not in [
-                GeneratedImageQualityTypes.hd,
-                GeneratedImageQualityTypes.standard,
-            ]:
-                return Response(
-                    {
-                        "details": "quality field '{}' is not one of ['standard', 'hd']".format(
-                            request_data.get("quality")
-                        )
-                    },
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            # Verificar campo 'gallery' y asignarlo si no existe
-            if not request_data.get("gallery", False):
-                try:
-                    gallery = Gallery.objects.get(user=request.user)
-                    request_data["gallery"] = gallery.pk
-                except Gallery.DoesNotExist:
-                    return Response(
-                        {"details": "Gallery not found for user."},
-                        status=status.HTTP_404_NOT_FOUND,
-                    )
-
-            # Validar y serializar los datos
-            serializer = self.serializer_class(data=request_data)
-            serializer.is_valid(raise_exception=True)
-
-            # Procesar creación de emote con transacción atómica
-            with transaction.atomic():
-                emote = self.perform_create(serializer)
-                image_manage = ImageGeneratorManager(
-                    user=request.user,
-                    image_object=emote,
-                    quality=request_data["quality"],
-                )
-                try:
-                    image_manage.process_images()
-                except Exception as e:
-                    return Response(
-                        {"details": f"Error processing emote images: {str(e)}"},
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    )
-
-            CalculateSpent.calculateSpent(image_object=emote, quality=request_data["quality"])
-            return Response({"emote": serializer.data}, status=status.HTTP_201_CREATED)
-
-        except ValidationError as e:
-            # Si hay un error de validación de datos
-            return Response({"details": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            # Captura errores no esperados
-            return Response(
-                {"details": f"Unexpected error: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+    def put(self, request, *args, **kwargs):
+        pass
+    
+    def
 
