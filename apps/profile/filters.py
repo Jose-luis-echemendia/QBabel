0521218bf44d0282a3f5001ef3831aef9fbacec1
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-02 21:27:02 -0500

    Create filters.py

---
+++ apps/profile/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from .models import Profile
+
+class EmoteFilter(django_filters.FilterSet):
+    me = django_filters.BooleanFilter(method='filter_me')
+    created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
+
+    class Meta:
+        model = Profile
+        fields = ["created_at", "me"]
+
+    def filter_me(self, queryset, name, value):
+        if value:
+            return queryset.filter(gallery__user=self.request.user)
+        return queryset
