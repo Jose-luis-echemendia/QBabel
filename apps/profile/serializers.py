bb976ad4830a82de64a06444abf47ba6c688fd37
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-05 10:00:58 -0500

    Update serializers.py

--- apps/profile/serializers.py
+++ apps/profile/serializers.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from apps.utils.utils import desconvertir_de_snake_case
 from apps.utils.serializers.abstract_serializers import AbstractBaseSerializer
 from apps.utils.models.models import GenericImage
+from apps.category.serializers import CategorySerializer
 from .models import Profile
 
 User = get_user_model()
@@ -14,6 +15,7 @@ class ProfileSerializer(AbstractBaseSerializer):
         queryset=GenericImage.objects.all(), write_only=True
     )
     avatar_details = serializers.SerializerMethodField()
+    literary_preferences_details = serializers.SerializerMethodField()
     
     class Meta:
         model = Profile
@@ -27,9 +29,16 @@ class ProfileSerializer(AbstractBaseSerializer):
             "country",
             "number_phone",
             "literary_preferences",
+            "literary_preferences_details",
         ]
+        extra_kwargs = {
+            "user": {"read_only": True},
+            "literary_preferences": {"required": False, "write_only": True},
+        }
         
     def get_avatar_details(self, obj):
         from apps.utils.serializers.serializers import ImageSerializer
         return ImageSerializer(obj.avatar).data if obj.avatar else None
 
+    def get_literary_preferences_details(self, obj):
+        return CategorySerializer(obj.literary_preferences, many=True).data
