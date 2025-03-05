5306e4fa38e598a5f55cd9fb111a136d337fd1a8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-05 11:36:37 -0500

    Update profileAPI.js

--- src/api/profileAPI.js
+++ src/api/profileAPI.js
@@ -12,6 +12,30 @@ export const getProfilesAPI = async () => {
       }
 }
 
+// endpoint for update profile
+export const updateProfileAPI = async (data) => {
+    try {
+        const response = await axiosInstance.put("/api/profile/", data);
+        
+        return response;
+      } catch (error) {
+        console.error("Error en actualizar el perfil:", error.response?.data || error.message);
+        throw error;
+      }
+}
+
+// endpoint for partial update profile
+export const updatePartialProfileAPI = async (data) => {
+    try {
+        const response = await axiosInstance.patch("/api/profile/", data);
+        
+        return response;
+      } catch (error) {
+        console.error("Error en actualizar parcialmente el perfil:", error.response?.data || error.message);
+        throw error;
+      }
+}
+
 // endpoint for get profile by id
 export const getProfileByIdAPI = async (id) => {
   try {
