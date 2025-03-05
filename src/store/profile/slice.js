bcf51f5bf89e979eba6b15d6f9f2da2f3bebdbec
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-05 11:48:39 -0500

    Update slice.js

--- src/store/profile/slice.js
+++ src/store/profile/slice.js
@@ -22,7 +22,77 @@ export const profileSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
+        // **Get profiles reducers**
+            .addCase(getProfilesThunk.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getProfilesThunk.fulfilled, (state, action) => {
+                state.loading = false
+                state.profiles = action.payload.results
+                state.count = action.payload.count
+                state.next = action.payload.next
+                state.previous = action.payload.previous
+            })
+            .addCase(getProfilesThunk.rejected, (state) => {
+                state.loading = false
+            })
         
+        // **Get profile by id reducers**
+            .addCase(getProfileByIdThunk.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getProfileByIdThunk.fulfilled, (state, action) => {
+                state.loading = false
+                state.profile = action.payload
+            })
+            .addCase(getProfileByIdThunk.rejected, (state) => {
+                state.loading = false
+            })
+
+        // **Get authenticated user profile reducers**
+            .addCase(getAuthenticatedUserProfileThunk.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getAuthenticatedUserProfileThunk.fulfilled, (state, action) => {
+                state.loading = false
+                state.profile = action.payload
+            })
+            .addCase(getAuthenticatedUserProfileThunk.rejected, (state) => {
+                state.loading = false
+            })
+
+        // **Update profile reducers**
+            .addCase(updateProfileThunk.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(updateProfileThunk.fulfilled, (state, action) => {
+                state.loading = false
+                state.profile = action.payload
+                state.profiles = state.profiles.map(profile => {
+                    if (profile.id === action.payload.id) return action.payload
+                    return profile
+                })
+
+            })
+            .addCase(updateProfileThunk.rejected, (state) => {
+                state.loading = false
+            })
+
+        // **Update partial profile reducers**
+            .addCase(updatePartialProfileThunk.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(updatePartialProfileThunk.fulfilled, (state, action) => {
+                state.loading = false
+                state.profile = action.payload
+                state.profiles = state.profiles.map(profile => {
+                    if (profile.id === action.payload.id) return action.payload
+                    return profile
+                })
+            })
+            .addCase(updatePartialProfileThunk.rejected, (state) => {
+                state.loading = false
+            })
     }
 })
 
