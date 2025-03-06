0668af698ace56635a85105d0b04458ac63b2181
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 18:39:33 -0500

    Update index.jsx

--- src/views/home-page/carousel/index.jsx
+++ src/views/home-page/carousel/index.jsx
@@ -27,9 +27,9 @@ const items = [
 export const CustomCarousel = () => {
   return (
     <>
-      <Carousel className="rounded-xl">
-        {items.map((item) => (
-          <figure key={uuid()} className="p-4 w-full h-96">
+      <Carousel className="rounded-xl" loop={true} autoplay={true} autoplayDelay={5000}>
+      {items.map((item) => (
+          <figure key={uuid()} className="w-full h-96">
             <img
               src={item.src}
               alt={item.title}
@@ -37,23 +37,6 @@ export const CustomCarousel = () => {
             />
           </figure>
         ))}
-      </Carousel>
-      <Carousel className="rounded-xl">
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
       </Carousel>
     </>
   );
