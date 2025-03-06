f60abe4b493de7c2ac83b59c8fd2ec13ff21b0b6
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 18:31:33 -0500

    Update index.jsx

--- src/views/home-page/carousel/index.jsx
+++ src/views/home-page/carousel/index.jsx
@@ -1,41 +1,60 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { v4 as uuid } from "uuid";
+import { Carousel } from "@material-tailwind/react";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import './styles.css';
-
-// import required modules
-import { Pagination, Navigation } from 'swiper/modules';
-
+const items = [
+  {
+    title: "Título del libro",
+    sumary: "pequeña descrioción de la historia o drama que trata el libro",
+    src: "/assets/images/2.jpg",
+  },
+  {
+    title: "Título del libro",
+    sumary: "pequeña descrioción de la historia o drama que trata el libro",
+    src: "/assets/images/3.jpg",
+  },
+  {
+    title: "Título del libro",
+    sumary: "pequeña descrioción de la historia o drama que trata el libro",
+    src: "/assets/images/2.jpg",
+  },
+  {
+    title: "Título del libro",
+    sumary: "pequeña descrioción de la historia o drama que trata el libro",
+    src: "/assets/images/3.jpg",
+  },
+];
 
 export const CustomCarousel = () => {
   return (
     <>
-        <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <Carousel className="rounded-xl">
+        {items.map((item) => (
+          <figure key={uuid()} className="p-4 w-full h-96">
+            <img
+              src={item.src}
+              alt={item.title}
+              className="h-full w-full object-cover"
+            />
+          </figure>
+        ))}
+      </Carousel>
+      <Carousel className="rounded-xl">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+      </Carousel>
     </>
-  )
-}
+  );
+};
