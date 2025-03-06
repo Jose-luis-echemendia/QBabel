717c3a97bec65768040c926b5a2d033c5c87586d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 16:33:46 -0500

    Update avatar.jsx

--- src/views/navbar/auth-links/avatar.jsx
+++ src/views/navbar/auth-links/avatar.jsx
@@ -35,56 +35,61 @@ const profileMenuItems = [
 
 export const CustomAvatar = ({ imageAvatar, user }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-    console.log(user)
+  console.log(user);
   const closeMenu = () => setIsMenuOpen(false);
   return (
     <>
-      <Menu open={isMenuOpen} handler={setIsMenuOpen}>
-        <MenuHandler>
-          <Button
-            variant="text"
-            color="blue-gray"
-            className="flex items-center rounded-full p-0"
-          >
-            {" "}
-            <div className="flex items-center gap-4 mr-5">
-              <Avatar src={imageAvatar.image} alt={imageAvatar.alt} />
-              <div>
-                <Typography variant="h6" className="text-primary"> {user.user_name} </Typography>
+      <div className="mr-10">
+        <Menu open={isMenuOpen} handler={setIsMenuOpen} className="">
+          <MenuHandler>
+            <Button
+              variant="text"
+              color="blue-gray"
+              className="flex items-center rounded-full p-0"
+            >
+              {" "}
+              <div className="flex items-center gap-4 mr-5">
+                <Avatar src={imageAvatar.image} alt={imageAvatar.alt} />
+                <div>
+                  <Typography variant="h6" className="text-primary">
+                    {" "}
+                    {user.user_name}{" "}
+                  </Typography>
+                </div>
               </div>
-            </div>
-          </Button>
-        </MenuHandler>
-        <MenuList className="">
-          {profileMenuItems.map(({ label, icon }, key) => {
-            const isLastItem = key === profileMenuItems.length - 1;
-            return (
-              <MenuItem
-                key={label}
-                onClick={closeMenu}
-                className={`flex items-center gap-2 rounded ${
-                  isLastItem
-                    ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                    : ""
-                }`}
-              >
-                {/* {React.createElement(icon, {
+            </Button>
+          </MenuHandler>
+          <MenuList className="">
+            {profileMenuItems.map(({ label, icon }, key) => {
+              const isLastItem = key === profileMenuItems.length - 1;
+              return (
+                <MenuItem
+                  key={label}
+                  onClick={closeMenu}
+                  className={`flex items-center gap-2 rounded ${
+                    isLastItem
+                      ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                      : ""
+                  }`}
+                >
+                  {/* {React.createElement(icon, {
                                 className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
                                 strokeWidth: 2,
                             })} */}
-                <Typography
-                  as="span"
-                  variant="small"
-                  className="font-normal"
-                  color={isLastItem ? "red" : "inherit"}
-                >
-                  {label}
-                </Typography>
-              </MenuItem>
-            );
-          })}
-        </MenuList>
-      </Menu>
+                  <Typography
+                    as="span"
+                    variant="small"
+                    className="font-normal"
+                    color={isLastItem ? "red" : "inherit"}
+                  >
+                    {label}
+                  </Typography>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </Menu>
+      </div>
     </>
   );
 };
