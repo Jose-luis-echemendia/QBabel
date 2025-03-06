212016ccc7f31ac5f75e240dedf9b0a37d8734f4
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 14:52:39 -0500

    Update avatar.jsx

--- src/views/navbar/auth-links/avatar.jsx
+++ src/views/navbar/auth-links/avatar.jsx
@@ -1,6 +1,20 @@
+import { Avatar as Av, Typography } from "@material-tailwind/react";
 
 export const Avatar = () => {
   return (
-    <div>Avatar</div>
-  )
-}
+    <>
+      <div className="flex items-center gap-4">
+        <Av
+          src="https://docs.material-tailwind.com/img/face-2.jpg"
+          alt="avatar"
+        />
+        <div>
+          <Typography variant="h6">Tania Andrew</Typography>
+          <Typography variant="small" color="gray" className="font-normal">
+            Web Developer
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+};
