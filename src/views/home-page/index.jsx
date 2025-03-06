ef77562582a660d849f20bae67300ca133fe3ecd
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 16:14:14 -0500

    Update index.jsx

--- src/views/home-page/index.jsx
+++ src/views/home-page/index.jsx
@@ -1,3 +1,5 @@
+import { CustomCarousel } from "./carousel";
+import { ContainerHome } from "@/containers/home";
 
 const HomeView = () => {
   return (
@@ -6,7 +8,20 @@ const HomeView = () => {
         <h2 id="footer-heading" className="sr-only">
           Home
         </h2>
-        <h2>sdfdf</h2>
+        <ContainerHome>
+          <p>
+          Lo más interesante de la semana
+          </p>
+          <CustomCarousel />
+          <p>
+            Historias gratis escogidas por la comunidad
+          </p>
+          <p>
+            historias completadas
+          </p>
+          <p>recomendaciones para ti</p>
+          <p>lo mejor de romance y amor</p>
+        </ContainerHome>
       </div>
     </>
   );
