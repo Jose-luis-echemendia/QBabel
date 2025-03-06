542e990924e0e79840bddffbebe4a7b4a591ddc0
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 14:42:38 -0500

    Update useProfile.js

---
+++ src/hooks/redux/useProfile.js
@@ -0,0 +1,13 @@
+import {
+  getProfilesThunk,
+  getProfileByIdThunk,
+  updateProfileThunk,
+  updatePartialProfileThunk,
+} from "@/store/profile/thunks";
+import { useAppDispatch } from "./useStore";
+
+export const useProfile = () => {
+  const dispath = useAppDispatch();
+
+  c;
+};
