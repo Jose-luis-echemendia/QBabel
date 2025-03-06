b7301f9accc06d4ee5590d7dd59b503097845e8f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 16:23:40 -0500

    Create index.jsx

---
+++ src/layout/authenticated-layout/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import LoadSuspense from '@/components/load-suspense';
+import CustomFooter  from "@/components/footer";
+import NavBar from '@/views/navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AuthenticatedLayout = ({ children }) => {
+  return (
+    <LoadSuspense className="relative flex flex-col ">
+      <NavBar />
+      <ToastContainer autoClose={5000} />
+      <main className='antialiased'>{children}</main>
+      <CustomFooter textColor="text-black"/>
+    </LoadSuspense>
+  );
+};
+
+AuthenticatedLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthenticatedLayout;
