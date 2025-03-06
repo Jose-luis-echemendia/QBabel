1c06972c5322a5463a5abda12bc5e9de8c1b66f8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-06 15:02:18 -0500

    Update index.jsx

--- src/views/navbar/auth-links/index.jsx
+++ src/views/navbar/auth-links/index.jsx
@@ -1,12 +1,14 @@
 import { GetAuthLinks } from "./get-auth-links";
-import { Avatar } from "./avatar";
+import { CustomAvatar } from "./avatar";
 import { useAppSelector } from "@/hooks/redux/useStore";
+import { AvatarDropdown } from "@/components/avatar/AvatarDropdown";
 
 export const AuthLinks = () => {
   const auth = useAppSelector((state) => state.auth);
   return (
     <>
-      <div>{auth.isAuthenticated ? <Avatar/> : <GetAuthLinks/>}</div>
+      <AvatarDropdown/>
+      <div>{auth.isAuthenticated ? <CustomAvatar/> : <GetAuthLinks/>}</div>
     </>
   );
 };
