39391148210d891f91ff27894186d219028634ff
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-07 18:33:54 -0500

    Update index.jsx

--- src/views/navbar/sidebar/index.jsx
+++ src/views/navbar/sidebar/index.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-import '@components/i18n'
-import classNames from 'classnames'
 import SidebarAccordion from '@views/sidebar/accordion-items/Accordion'
-import { usePathname } from 'next/navigation'
 import { getSidebarElements } from './sidebar-data'
-import { useTranslation } from 'react-i18next'
+import { IconLogo } from '@/components/icons/log-icon'
+
 
 const CustomSideBar = ({ className }) => {
-  const { t } = useTranslation('sidebar')
-  const pathName = usePathname()
-  // Generar elementos de la barra lateral dinámicamente
+
+
   const sideBarElements = getSidebarElements(t, pathName)
 
   console.log(t('orders_defectiveProducts', { returnObjects: true }))
 
   return (
     <aside className={classNames('relative py-[20px]', className)}>
-      <Image
-        src='/assets/images/logo.svg'
-        width={126}
-        height={32}
-        alt='logo'
-        className='mx-auto mb-9'
-      />
+      <IconLogo size="80" />
       <SidebarAccordion elements={sideBarElements} />
     </aside>
   )
