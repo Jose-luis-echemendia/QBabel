90362b4a7b65838d5f57f4f3cc35ab923f5103ba
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-07 08:29:14 -0500

    Create index.jsx

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,21 @@
+import { lazy } from "react";
+import LoadSuspense from "@/components/load-suspense";
+import AuthenticatedLayout from "@/layout/authenticated-layout";
+import { CustomSidebar } from "@/views/navbar/sidebar";
+
+const AdminView = lazy(() => import("@/views/admin"));
+
+const AdminPage = () => {
+  return (
+    <LoadSuspense>
+      <AuthenticatedLayout>
+        <div className="grid grid-cols-12">
+        <CustomSidebar/>
+        <AdminView />
+        </div>
+      </AuthenticatedLayout>
+    </LoadSuspense>
+  );
+};
+
+export default AdminPage;
