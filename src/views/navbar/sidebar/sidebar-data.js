ed0c32cfbe283511264c7dae1b609ba84d7850c5
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-07 19:00:45 -0500

    Update sidebar-data.js

---
+++ src/views/navbar/sidebar/sidebar-data.js
@@ -0,0 +1,39 @@
+export const elements = [
+  {
+    id: 1,
+    label: 'Dashboard',
+    highlightPath: ['/dashboard'],
+    subElements: [],
+  },
+  {
+    id: 2,
+    label: 'Orders',
+    highlightPath: ['/orders', '/orders/defective-products'],
+    subElements: ['Defective Products'],
+  },
+  {
+    id: 3,
+    label: 'Products',
+
+    highlightPath: ['/products', '/products/add-product'],
+    subElements: ['Add Product'],
+  },
+  {
+    id: 4,
+    label: 'Customers',
+    highlightPath: ['/customers'],
+    subElements: [],
+  },
+  {
+    id: 5,
+    label: 'Reports',
+    highlightPath: ['/reports'],
+    subElements: [],
+  },
+  {
+    id: 6,
+    label: 'Settings',
+    highlightPath: ['/settings'],
+    subElements: [],
+  },
+]
