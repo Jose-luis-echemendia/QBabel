032400e688a4760af35f59fe92ca09d3679d6a0c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-07 20:08:46 -0500

    Update index.jsx

--- src/views/navbar/sidebar/index.jsx
+++ src/views/navbar/sidebar/index.jsx
@@ -1,13 +1,9 @@
 import SidebarAccordion from './accordion-items/Accordion'
-import { IconLogo } from '@/components/icons/logo-icon'
-
 
 const CustomSideBar = () => {
 
-
   return (
     <aside className="relative py-[20px]">
-      <IconLogo size="80" />
       <SidebarAccordion />
     </aside>
   )
