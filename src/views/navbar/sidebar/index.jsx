5f8945ea233e2caf63b035f8b6e2043e5b7a864b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-07 19:02:19 -0500

    Update index.jsx

--- src/views/navbar/sidebar/index.jsx
+++ src/views/navbar/sidebar/index.jsx
@@ -1,19 +1,14 @@
 import SidebarAccordion from './accordion-items/Accordion'
-import { getSidebarElements } from './sidebar-data'
 import { IconLogo } from '@/components/icons/logo-icon'
 
 
 const CustomSideBar = () => {
 
 
-  const sideBarElements = getSidebarElements(t, pathName)
-
-  console.log(t('orders_defectiveProducts', { returnObjects: true }))
-
   return (
     <aside className="relative py-[20px]">
       <IconLogo size="80" />
-      <SidebarAccordion elements={sideBarElements} />
+      <SidebarAccordion />
     </aside>
   )
 }
