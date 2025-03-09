86f64fae5badbed66dd0c61375a87c66cb8a7c43
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-09 18:26:07 -0400

    Update index.jsx

--- src/views/home-page/carousel/index.jsx
+++ src/views/home-page/carousel/index.jsx
@@ -1,6 +1,5 @@
 import { v4 as uuid } from "uuid";
-import { Carousel } from "@material-tailwind/react";
-import { customNavigation } from "./config";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,8 +7,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "./styles.css";
-
 import { Pagination } from "swiper/modules";
+
+
 const items = [
   {
     title: "Título del libro",
@@ -36,24 +36,6 @@ const items = [
 export const CustomCarousel = () => {
   return (
     <>
-      <Carousel
-        className="!overflow-hidden rounded-xl h-[400px]"
-        loop={true}
-        autoplay={true}
-        autoplayDelay={5000}
-        navigation={customNavigation}
-      >
-        {items.map((item) => (
-          <figure key={uuid()} className="w-full h-96">
-            <img
-              src={item.src}
-              alt={item.title}
-              className="h-full w-full object-cover"
-            />
-          </figure>
-        ))}
-      </Carousel>
-
       <Swiper  loop={true} modules={[Pagination]} className="mySwiper">
         {items.map((item) => (
           <SwiperSlide key={uuid()}>
