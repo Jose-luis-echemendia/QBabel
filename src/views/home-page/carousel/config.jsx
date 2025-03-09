a69ca19e63c5a0366b29106cde012d682f9468f9
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-09 17:18:55 -0400

    Update config.jsx

--- src/views/home-page/carousel/config.jsx
+++ src/views/home-page/carousel/config.jsx
@@ -1,14 +1,16 @@
 export const PrevArrow = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    fill="none"
     viewBox="0 0 24 24"
-    fill="currentColor"
-    className="size-14"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-10"
   >
     <path
-      fillRule="evenodd"
-      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z"
-      clipRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 19.5 8.25 12l7.5-7.5"
     />
   </svg>
 );
@@ -17,14 +19,16 @@ export const PrevArrow = () => (
 export const NextArrow = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    fill="none"
     viewBox="0 0 24 24"
-    fill="currentColor"
-    className="size-14"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-10"
   >
     <path
-      fillRule="evenodd"
-      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z"
-      clipRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m8.25 4.5 7.5 7.5-7.5 7.5"
     />
   </svg>
 );
