98646c1290dee62e2988e18cec09bcff7875263d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-08 21:32:00 -0500

    Update index.jsx

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,7 +1,7 @@
 
 export const ContainerHome = ({children}) => {
   return (
-    <div className="container mx-auto flex flex-col px-16 py-0">
+    <div className="container mx-auto flex flex-col px-16 py-0 gap-14">
         {children}
     </div>
   )
