74b1e9e07db54edd6269bf94296920e70633705d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-10 11:01:37 -0400

    Update carousel-items.jsx

--- src/views/home-page/carousel/books/carousel-items.jsx
+++ src/views/home-page/carousel/books/carousel-items.jsx
@@ -15,23 +15,51 @@ export const BookGroup = ({ books, itemHeight }) => (
 );
 
 export const BookGroupCard = ({ books, itemHeight }) => (
-  <div className={`flex items-center justify-center gap-2`}>
+  <div className={`flex justify-center gap-3`}>
     {books.map((book) => (
       <div
         key={book.id}
-        className={`${itemHeight} bg-gray-100 flex gap-2  w-1/2 `}
+        className={`${itemHeight} bg-gray-100 flex gap-2  w-1/2 rounded-xl`}
       >
-        <figure className="">
+        <figure className="w-[800px]">
           <img
             src={book.img}
             alt={book.title}
             className="h-full w-full object-cover"
           />
         </figure>
-        <div className="flex flex-col gap-2 pt-3">
-          <h4> {book.tittle} </h4>
-          <p className="text-sm"> {book.description} </p>
-          <span>reviews</span>
+        <div className="flex flex-col gap-4 pt-3 px-2">
+          <h4 className="text-xl text-black font-opensans font-bold">
+            {" "}
+            {book.tittle}{" "}
+          </h4>
+          <p className="text-sm text-start text-gray-800">
+            {" "}
+            {book.description}{" "}
+          </p>
+          <span className="flex gap-1.5 text-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+
+            {book.reviews}
+          </span>
         </div>
       </div>
     ))}
