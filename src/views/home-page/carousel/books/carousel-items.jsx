cb52a1972f69961b07b811105a5d31883ef62650
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-10 18:06:46 -0400

    Update carousel-items.jsx

--- src/views/home-page/carousel/books/carousel-items.jsx
+++ src/views/home-page/carousel/books/carousel-items.jsx
@@ -3,12 +3,40 @@ import PropTypes from "prop-types";
 export const BookGroup = ({ books, itemHeight }) => (
   <div className="flex items-center justify-center gap-2 px-5">
     {books.map((book) => (
-      <figure key={book.id} className={`w-1/4 ${itemHeight}`}>
+      <figure
+        key={book.id}
+        className={`w-1/4 ${itemHeight} flex flex-col gap-1`}
+      >
         <img
           src={book.img}
           alt={book.title}
           className="h-full w-full object-cover rounded-xl"
         />
+        <span className="ml-2 bg-gray-200 w-fit p-0.5 mt-1 rounded-xl font-opensans text-black font-semibold">
+          {book.category}{" "}
+        </span>
+        <span className="ml-2 flex gap-1.5 text-gray-800">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>{" "}
+          {book.reviews}{" "}
+        </span>
       </figure>
     ))}
   </div>
