6fc8ee13074b247e826a4b40761b8cc46788e052
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-10 11:13:23 -0400

    Update carousel-items.jsx

--- src/views/home-page/carousel/books/carousel-items.jsx
+++ src/views/home-page/carousel/books/carousel-items.jsx
@@ -37,7 +37,7 @@ export const BookGroupCard = ({ books, itemHeight }) => (
             {" "}
             {book.description}{" "}
           </p>
-          <span className="flex gap-1.5 text-gray-800">
+          <span className="flex gap-1.5 -mt-1 text-gray-800">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
