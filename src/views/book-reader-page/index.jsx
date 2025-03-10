53faaf68e67e653733ee84a6a4827beeb236c42e
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-10 19:13:13 -0400

    Update index.jsx

--- src/views/book-reader-page/index.jsx
+++ src/views/book-reader-page/index.jsx
@@ -1,9 +1,11 @@
-
- const BookReaderView = () => {
+const BookReaderView = () => {
   return (
     <>
-      <section className="bg-gray-50 max-h-screen w-full h-full p-5">BookReaderView</section></>
-  )
-}
+      <section className="bg-gray-50 max-h-screen w-full h-full p-5">
+        BookReaderView
+      </section>
+    </>
+  );
+};
 
-export default BookReaderView
+export default BookReaderView;
