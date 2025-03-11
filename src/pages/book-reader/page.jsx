ea8d14dab546a91e5c6efcf5a719f03878b670d2
Abero-dev <abel04.mata@gmail.com>
2025-03-11 16:41:33 -0400

    config library-pdf

--- src/pages/book-reader/page.jsx
+++ src/pages/book-reader/page.jsx
@@ -1,5 +1,11 @@
 import { lazy } from "react";
 import LoadSuspense from "@/components/load-suspense";
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 const BookReaderView = lazy(() => import("@/views/book-reader-page"));
 
