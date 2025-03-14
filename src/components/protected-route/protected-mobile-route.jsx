650eab95ecb0793f83f8dacca37bd2b01def6f93
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-14 08:58:57 -0400

    Create protected-mobile-route.jsx

---
+++ src/components/protected-route/protected-mobile-route.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+export const ProtectedMobileRoute = ({ children, isAllowed, redirectTo = "/" }) => {
+    const isMobile = window.innerWidth <= 400;
+
+    if (!isAllowed)
+        return <Navigate to={redirectTo}></Navigate>;
+    
+      return children ? children : <Outlet />
+
+};
+
+ProtectedMobileRoute.propTypes = {
+  children: PropTypes.node,
+  isAllowed: PropTypes.bool.isRequired,
+  redirectTo: PropTypes.string
+};
+
