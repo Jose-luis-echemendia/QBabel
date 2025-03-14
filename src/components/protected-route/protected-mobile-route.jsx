fc9a53192c45ae546f1648bf9b6b67d414b9d05c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-14 09:22:32 -0400

    Update protected-mobile-route.jsx

--- src/components/protected-route/protected-mobile-route.jsx
+++ src/components/protected-route/protected-mobile-route.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const ProtectedMobileRoute = ({ children, isAllowed, redirectTo = "/" }) => {
     const isMobile = window.innerWidth <= 400;
 
-    if (!isAllowed)
+    if (!isAllowed || !isMobile)
         return <Navigate to={redirectTo}></Navigate>;
     
       return children ? children : <Outlet />
