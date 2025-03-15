b91785d8d33d78917cd6addf4ecb4ddbc5b98e66
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-14 20:45:27 -0400

    Update index.jsx

--- src/views/navbar/index.jsx
+++ src/views/navbar/index.jsx
@@ -2,94 +2,7 @@ import { IconLogo } from "@/components/icons/logo-icon";
 import { ExplorePopover } from "./popover-explore";
 import { CustomIcon } from "@/components/icons/custom-icons";
 import { AuthLinks } from "./auth-links";
-import { Link } from "react-router-dom";
-
-export const itemsNavbar = [
-  {
-    id: 1,
-    title: "Home",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-        />
-      </svg>
-    ),
-    link: "/",
-  },
-  {
-    id: 2,
-    title: "User",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-        />
-      </svg>
-    ),
-    link: "/AboutMe",
-  },
-  {
-    id: 3,
-    title: "Book",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-        />
-      </svg>
-    ),
-    link: "/services",
-  },
-  {
-    id: 4,
-    title: "Target",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-        />
-      </svg>
-    ),
-    link: "/Proyects",
-  },
-];
+import ResponsiveNav from "./responsive";
 
 const NavBar = () => {
   return (
@@ -124,21 +37,7 @@ const NavBar = () => {
           </div>
         </nav>
       </header>
-      <nav className="pt-5 -mb-10 lg:hidden">
-        <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
-          {itemsNavbar.map((item) => (
-            <div
-              key={item.id}
-              className="px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary hover:text-black"
-              data-tooltip-target="tooltip-default"
-            >
-              <Link href={item.link} className="bg-primary">
-                {item.icon}
-              </Link>
-            </div>
-          ))}
-        </div>
-      </nav>
+      <ResponsiveNav />
     </>
   );
 };
