b72ef69a8823c5025c2854e08a9daf9c1ad4b42c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-16 14:10:13 -0400

    Update index.jsx

--- src/views/footer/index.jsx
+++ src/views/footer/index.jsx
@@ -4,33 +4,29 @@ import { Decorator } from "./decorator";
 const Footer = () => {
   return (
     <footer className="w-full max-h-screen h-full p-10 relative flex justify-center items-center bg-[#2E2E2E] text-primary">
-      <section className="flex flex-col justify-center items-center w-full">
-        <div className="flex flex-col w-full justify-center items-center px-0">
-          <div className="col-span-3 flex flex-col justify-center items-center gap-10">
-            <figure className="border border-primary rounded-full p-0 m-0">
-              <img
-                src="/assets/images/home/footer/1.png"
-                alt="logo"
-                className="object-cover"
-              />
-            </figure>
-            <div className="flex justify-center items-center gap-10 mt-1">
-              <button className="bg-primary py-2 px-10 rounded-xl">
-                <span className="text-white-100 font-anton font-medium text-2xl">
-                  Leer
-                </span>
-              </button>
-              <button className="bg-primary py-2 px-10 rounded-xl">
-                <span className="text-black-500 font-anton font-medium text-2xl">
-                  Escribir
-                </span>
-              </button>
-            </div>
-            <h2 className="font-italianno mx-auto w-fit mt-2 text-[50px] leading-[55px] text-center">
-              Has realidad tu historia!
-            </h2>
-          </div>
+      <section className="flex flex-col gap-6 justify-center items-center w-full">
+        <figure className="border border-primary rounded-full p-0 m-0">
+          <img
+            src="/assets/images/home/footer/1.png"
+            alt="logo"
+            className="object-cover"
+          />
+        </figure>
+        <div className="flex justify-center items-center gap-10 mt-1">
+          <button className="bg-primary py-2 px-10 rounded-xl">
+            <span className="text-white-100 font-anton font-medium text-2xl">
+              Leer
+            </span>
+          </button>
+          <button className="bg-primary py-2 px-10 rounded-xl">
+            <span className="text-black-500 font-anton font-medium text-2xl">
+              Escribir
+            </span>
+          </button>
         </div>
+        <h2 className="font-italianno mx-auto w-fit mt-2 text-[50px] leading-[55px] text-center">
+          Has realidad tu historia!
+        </h2>
         <CustomFooter />
       </section>
       <Decorator />
