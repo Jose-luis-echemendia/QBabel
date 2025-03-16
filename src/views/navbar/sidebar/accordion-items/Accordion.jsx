2294132752a9e0af41615685c322f8f54ce6ada7
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-16 17:23:30 -0400

    Update Accordion.jsx

--- src/views/navbar/sidebar/accordion-items/Accordion.jsx
+++ src/views/navbar/sidebar/accordion-items/Accordion.jsx
@@ -1,114 +1,120 @@
-import { useState } from 'react'
-import { Accordion, AccordionHeader, AccordionBody, List, ListItem } from '@material-tailwind/react'
-import { IconCollapse } from '@/components/icons/custom-icons'
-import PropTypes from 'prop-types'
-import { v4 as uuidv4 } from 'uuid'
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  List,
+  ListItem,
+} from "@material-tailwind/react";
+import { IconCollapse } from "@/components/icons/custom-icons";
 import { IconDashboard } from "@/components/icons/custom-icons";
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from "prop-types";
 
 const elements = [
   {
     id: 1,
-    label: 'General',
-    highlightPath: ['/overview'],
+    label: "General",
+    highlightPath: ["/admin"],
     icon: <IconDashboard />,
     subElements: [],
   },
   {
     id: 2,
-    label: 'Usuarios',
-    highlightPath: ['/overview'],
+    label: "Usuarios",
+    highlightPath: ["/admin/users"],
     icon: <IconDashboard />,
     subElements: [],
   },
   {
     id: 3,
-    label: 'Libros',
-    highlightPath: ['/orders', '/orders/defective-products'],
+    label: "Libros",
+    highlightPath: ["/admin/books", "/orders/defective-products"],
     icon: <IconDashboard />,
-    subElements: ['Defective Products'],
+    subElements: ["Defective Products"],
   },
   {
     id: 4,
-    label: 'Categorías',
-    highlightPath: ['/products', '/products/add-product'],
+    label: "Categorías",
+    highlightPath: ["/admin/cateogies", "/products/add-product"],
     icon: <IconDashboard />,
-    subElements: ['Add Product'],
+    subElements: ["Add Product"],
   },
   {
     id: 5,
-    label: 'Ventas',
-    highlightPath: ['/customers'],
+    label: "Ventas",
+    highlightPath: ["/admin/sales"],
     icon: <IconDashboard />,
     subElements: [],
   },
   {
     id: 6,
-    label: 'Publicaciones',
-    highlightPath: ['/reports'],
+    label: "Publicaciones",
+    highlightPath: ["/admin/posts"],
     icon: <IconDashboard />,
     subElements: [],
   },
   {
     id: 7,
-    label: 'Soporte profesional',
-    highlightPath: ['/settings'],
+    label: "Soporte profesional",
+    highlightPath: ["/admin/profesional-suport"],
     icon: <IconDashboard />,
     subElements: [],
   },
   {
     id: 8,
-    label: 'Denuncias',
-    highlightPath: ['/settings'],
+    label: "Denuncias",
+    highlightPath: ["/admin/complaints"],
     icon: <IconDashboard />,
     subElements: [],
   },
-]
-
+];
 
 const SidebarAccordion = () => {
-  const [open, setOpen] = useState(0)
+  const [open, setOpen] = useState(0);
 
   const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value)
-  }
-
-
+    setOpen(open === value ? 0 : value);
+  };
 
   const sideBarElementsStyle =
-    'font-poppins text-[14px] text-center font-semibold leading-[28px] text-[#A0AEC0]'
+    "font-poppins text-[14px] text-center font-semibold leading-[28px] text-[#A0AEC0]";
 
   return (
-    <div className='w-[300px]'>
+    <div className="w-[300px]">
       {elements.map((element) => (
         <Accordion
-          className='relative'
+          className="relative"
           key={element.id}
           open={open === element.id}
           icon={
             element.subElements.length !== 0 && (
               <IconCollapse
                 className={`mx-auto transition-transform ${
-                  open === element.id ? 'rotate-180' : ''
+                  open === element.id ? "rotate-180" : ""
                 }`}
               />
             )
           }
         >
           {/* ------------------------------------- */}
-          <ListItem className='p-0' selected={open === element.id}>
-            <AccordionHeader onClick={() => handleOpen(element.id)} className='border-b-0 p-3'>
+          <ListItem className="p-0" selected={open === element.id}>
+            <AccordionHeader
+              onClick={() => handleOpen(element.id)}
+              className="border-b-0 p-3"
+            >
               {element.icon}
-              <span>
-                {element.label}
-              </span>
+              <span>{element.label}</span>
             </AccordionHeader>
           </ListItem>
           {element.subElements.length > 0 && (
-            <AccordionBody className='py-1'>
-              <List className='p-0'>
+            <AccordionBody className="py-1">
+              <List className="p-0">
                 {element.subElements.map((subElement) => (
                   <ListItem key={uuidv4()}>
-                    <span className={`${sideBarElementsStyle} ml-[30%]`}>{subElement}</span>
+                    <span className={`${sideBarElementsStyle} ml-[30%]`}>
+                      {subElement}
+                    </span>
                   </ListItem>
                 ))}
               </List>
@@ -117,10 +123,10 @@ const SidebarAccordion = () => {
         </Accordion>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default SidebarAccordion
+export default SidebarAccordion;
 
 SidebarAccordion.propTypes = {
   elements: PropTypes.arrayOf(
@@ -128,7 +134,7 @@ SidebarAccordion.propTypes = {
       id: PropTypes.number.isRequired,
       label: PropTypes.string.isRequired,
       subElements: PropTypes.arrayOf(PropTypes.string),
-      icon: PropTypes.element.isRequired
+      icon: PropTypes.element.isRequired,
     })
-  ).isRequired
-}
+  ).isRequired,
+};
