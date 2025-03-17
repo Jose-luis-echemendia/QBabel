09649768371f5e0d68ef733f71bee85866e0088d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-16 22:40:46 -0400

    Update page.jsx

--- src/pages/admin/admin-users/page.jsx
+++ src/pages/admin/admin-users/page.jsx
@@ -1,24 +1,15 @@
 import { lazy } from "react";
 import LoadSuspense from "@/components/load-suspense";
-import AuthenticatedLayout from "@/layout/authenticated-layout";
+import AdminLayout from "@/layout/admin-layout";
 
 const AdminUserView = lazy(() => import("@/views/admin-page/admin-user"));
-const SidebarView = lazy(() => import("@/views/navbar/sidebar"));
 
 const AdminUserPage = () => {
   return (
     <LoadSuspense>
-      <AuthenticatedLayout classNameFooter={"pl-72"}>
-        <div className="grid grid-cols-12 ml-10 mt-5 rounded-xl">
-          <div className="col-span-2 -ml-8">
-            <SidebarView />
-          </div>
-          <div className="col-span-10 relative -mt-1">
-            <AdminUserView />
-            <div className="absolute bg-gray-50 w-full h-20 -bottom-20 z-[-1] border-t" />
-          </div>
-        </div>
-      </AuthenticatedLayout>
+      <AdminLayout>
+        <AdminUserView />
+      </AdminLayout>
     </LoadSuspense>
   );
 };
