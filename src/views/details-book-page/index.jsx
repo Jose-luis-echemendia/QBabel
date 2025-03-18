90adb6d9ee64290e6779f7fd66093fc0d9374ee8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-18 16:09:54 -0400

    Update index.jsx

--- src/views/details-book-page/index.jsx
+++ src/views/details-book-page/index.jsx
@@ -1,8 +1,114 @@
 const DetailsBookView = () => {
+  const book = {
+    id: 5,
+    author: "Antoine De Saint-Exupéry",
+    tittle: "El Principito",
+    img: "/assets/images/home/best_books/book5.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,rem magnam nesciunt minima placeat, itaque eum neque officiis unde,eaque optio ratione aliquid assumenda facere ab et quasi ducimus autdoloribus non numquam. Lorem ipsum dolor, sit amet consecteturadipisicing elit. Quisquam quidem perferendis ab doloribus ipsamratione fugit officia, rem necessitatibus.",
+    color: "#A5A5A6",
+    reviews: "41k",
+    reads: "7.6M",
+    parts: 71,
+    category: "category",
+  };
+
   return (
     <>
-      <article className="max-h-screen w-full h-full p-5">
-        <header></header>
+      <article className="max-h-screen w-full h-full py-5 mx-0">
+        <header className="w-full h-[350px] flex items-center justify-center border-b shadow-2xl">
+          <figure className="flex gap-8 items-center justify-center w-full h-full">
+            <img
+              src={book.img}
+              alt={book.tittle}
+              className="object-cover rounded-xl shadow-xl h-[300px] w-[200px] -mt-2.5"
+            />
+            <figcaption className="flex flex-col gap-12">
+              <h2>{book.tittle}</h2>
+              <div className="flex gap-10 justify-center items-center">
+                <div className="flex flex-col gap-1.5 items-center justify-center">
+                  <span className="flex gap-1.5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                    <small>Lecturas</small>
+                  </span>
+                  <span>{book.reads} </span>
+                </div>
+                <div className="flex flex-col gap-1.5 items-center justify-center">
+                  <span className="flex gap-1.5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                    <small>Votos</small>
+                  </span>
+                  <span> {book.reviews} </span>
+                </div>
+                <div className="flex flex-col gap-1.5 items-center justify-center">
+                  <span className="flex gap-1.5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                    <small>Capítulos</small>
+                  </span>
+                  <span> {book.parts} </span>
+                </div>
+              </div>
+              <div>
+                <button>Comenzar a leer</button>
+                <button>Agregar</button>
+              </div>
+            </figcaption>
+          </figure>
+        </header>
         <footer></footer>
       </article>
     </>
