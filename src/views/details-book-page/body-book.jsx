35c34df85d1abe72da97a5fb87d1686542ee09ec
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-18 20:01:05 -0400

    ok

---
+++ src/views/details-book-page/body-book.jsx
@@ -0,0 +1,7 @@
+
+
+export const CustomBodyBood = () => {
+  return (
+    <>CustomBodyBood</>
+  )
+}
---
+++ src/views/details-book-page/header-book.jsx
@@ -0,0 +1,8 @@
+
+
+export const CustomHeaderBook = () => {
+  return (
+    <>
+    </>
+  )
+}
