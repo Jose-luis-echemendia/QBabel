0a858a956d263128cdc8e6258fa9eb3e83d9753e
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-18 23:38:09 -0400

    Update related-information-book.jsx

--- src/views/details-book-page/related-information-book.jsx
+++ src/views/details-book-page/related-information-book.jsx
@@ -1,7 +1,12 @@
-
-
 export const RelatedInformationBook = () => {
   return (
-    <div>RelatedInformationBook</div>
-  )
-}
+    <>
+      <div className="col-span-2 py-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quasi,
+        enim quia voluptatum cumque sunt dolorem, harum animi tempora cum
+        dignissimos voluptatem alias quisquam soluta atque error, omnis iure
+        quaerat!
+      </div>
+    </>
+  );
+};
