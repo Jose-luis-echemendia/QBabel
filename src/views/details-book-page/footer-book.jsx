6bc587260215b907343d1bd0f6a8c01a5882bff8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-18 23:55:09 -0400

    Update footer-book.jsx

--- src/views/details-book-page/footer-book.jsx
+++ src/views/details-book-page/footer-book.jsx
@@ -1,7 +1,9 @@
 export const CustomFooterBook = ({ book }) => {
   return (
     <>
-      <footer className="col-span-4 flex flex-col justify-start items-start -mt-10">footer</footer>
+      <footer className="col-span-4 flex flex-col justify-start items-start -mt-10">
+        <h6 className="font-semibold text-xl">Tabla de contenidos</h6>
+      </footer>
     </>
   );
 };
