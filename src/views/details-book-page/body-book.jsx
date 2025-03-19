eb5d249976c9f48f799f4e1904d061d84c815a59
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-18 23:38:03 -0400

    Update body-book.jsx

--- src/views/details-book-page/body-book.jsx
+++ src/views/details-book-page/body-book.jsx
@@ -1,7 +1,43 @@
 export const CustomBodyBook = ({ book }) => {
   return (
     <>
-      <div></div>
+      <div className="col-span-4 flex flex-col justify-start items-start py-10 gap-6">
+        <figure className="flex gap-5 justify-center items-center">
+          <img
+            src={book.author.avatar}
+            alt={book.author.name}
+            className="w-10 h-10 object-cover rounded-full"
+          />
+          <figcaption>{book.author.name}</figcaption>
+        </figure>
+
+        {book.isComplete && (
+          <div className="p-2 rounded-xl bg-green-800 -ml-[1px]">
+            <span className="text-white-100 font-semibold">Completada</span>
+          </div>
+        )}
+        <div className="">
+          {book.description}
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          quas ipsa, vitae ipsum ipsam nihil consectetur non ratione eligendi.
+          Quia minima deserunt dolorum earum. Harum itaque officia architecto
+          libero veniam!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          quas ipsa, vitae ipsum ipsam nihil consectetur non ratione eligendi.
+          Quia minima deserunt dolorum earum. Harum itaque officia architecto
+          libero veniam!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          quas ipsa, vitae ipsum ipsam nihil consectetur non ratione eligendi.
+          Quia minima deserunt dolorum earum. Harum itaque officia architecto
+          libero veniam!
+        </div>
+      </div>
     </>
   );
 };
