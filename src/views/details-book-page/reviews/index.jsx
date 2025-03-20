1956c2e4d719ff4267c1ddcf97c4f7b00d5c9087
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-20 02:03:26 -0400

    Update index.jsx

--- src/views/details-book-page/reviews/index.jsx
+++ src/views/details-book-page/reviews/index.jsx
@@ -3,7 +3,7 @@ import { Reviews } from "./reviews";
 
 export const ReviewsBook = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-0">
       <ReviewForm />
       <Reviews />
     </div>
