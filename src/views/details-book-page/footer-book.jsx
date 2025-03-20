cf305f72fd6795e909e77501a1166e28b9ab6f89
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-19 21:14:25 -0400

    Update footer-book.jsx

--- src/views/details-book-page/footer-book.jsx
+++ src/views/details-book-page/footer-book.jsx
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 export const CustomFooterBook = ({ book }) => {
   return (
     <>
-      <footer className="col-span-4 flex flex-col gap-5 justify-start items-start -mt-10 shadow-2xl shadow-gray-400 py-4 px-6 rounded-2xl">
+      <footer className="flex flex-col gap-5 justify-start items-start -mt-10 shadow-2xl shadow-gray-400 py-4 px-6 rounded-2xl">
         <h6 className="font-semibold text-xl">Tabla de contenidos</h6>
         {book.chapters.map((chapter) => (
           <div key={uuid()}> {chapter.name} </div>
