5b6270c5d30a0cfda4b4268346d9925275158ada
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 11:00:14 -0400

    Update categories.jsx

--- src/views/category-book-page/categories.jsx
+++ src/views/category-book-page/categories.jsx
@@ -1,9 +1,16 @@
 import { schemaImagesCategoria } from "@/views/welcome-page/categories/images-categoria";
+import { v4 as uuidi } from "uuid";
 
 export const Categories = () => {
   return (
     <>
-      <div>{}</div>
+      <div className="flex flex-wrap justify-center items-center w-full h-full">
+        {schemaImagesCategoria.map((category) => (
+          <div key={uuidi()} className="bg-gay-600 py-2 px-5">
+            <span className="text-black">{category.text}</span>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
