4a514de9784b2ae1d1cc379bd247ad8e2d219731
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 08:45:54 -0400

    Update best-sellers.jsx

--- src/views/category-book-page/best-sellers.jsx
+++ src/views/category-book-page/best-sellers.jsx
@@ -1,11 +1,12 @@
-import { Decorator } from "./decorator"
+import { Decorator } from "./decorator";
 
 export const BestSellers = () => {
   return (
     <>
-        <div className="relative">
-            <Decorator/>
-        </div>
+      <div className="relative w-full h-full flex flex-col items-start justify-start gap-10">
+        <h3 className="px-20 ml-5 font-semibold text-xl">Top 10 libros más vendidos</h3>
+        <Decorator />
+      </div>
     </>
-  )
-}
+  );
+};
