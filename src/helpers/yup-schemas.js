ef54a7ae692721ed194ceef2ed5a62ef96740126
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 08:58:06 -0400

    Update yup-schemas.js

--- src/helpers/yup-schemas.js
+++ src/helpers/yup-schemas.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-const schemaLogin = yup.object({
+export const schemaLogin = yup.object({
   email: yup.string().email("Email inválido").required("El email es obligatorio"),
   password: yup.string().min(6, "La contraseña debe tener al menos 6 caracteres").required("La contraseña es obligatoria"),
 });
