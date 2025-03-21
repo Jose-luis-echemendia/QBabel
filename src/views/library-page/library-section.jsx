6271c8b2a59766a888995c300354086ce8ec2aec
Andy Torres <andy18wrld@gmail.com>
2025-03-21 11:03:27 -0400

    a

--- src/views/library-page/library-section.jsx
+++ src/views/library-page/library-section.jsx
@@ -1,6 +1,9 @@
+// LibrarySection.jsx
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import AllStories from './AllStories';
+// Importa tu componente para Reading Lists
+import { ReadingListView } from './reading-view';
 
 function LibrarySection({ books }) {
   const [activeTab, setActiveTab] = useState('current');
@@ -8,8 +11,9 @@ function LibrarySection({ books }) {
   return (
     <section className='py-4 '>
       <div className='max-w-[1200px] mx-auto px-4 '>
-        <h2 className=' relative  text-3xl font-bold mb-4 mt-20  '>Library</h2>
+        <h2 className='relative text-3xl font-bold mb-4 mt-20'>Library</h2>
 
+        {/* Tabs */}
         <div className='flex items-center justify-between border-b border-gray-200 mb-4'>
           <div className='flex space-x-6 text-xl'>
             <button
@@ -25,7 +29,7 @@ function LibrarySection({ books }) {
             <button
               className={`pb-2 ${
                 activeTab === 'archive'
-                  ? 'border-b-2 border-primary  font-bold'
+                  ? 'border-b-2 border-primary font-bold'
                   : 'text-gray-600'
               }`}
               onClick={() => setActiveTab('archive')}
@@ -45,17 +49,21 @@ function LibrarySection({ books }) {
           </div>
         </div>
 
+        {/* Contenido según la pestaña */}
         {activeTab === 'current' && (
           <>
             <h3 className='text-xl font-bold mb-7'>All Stories</h3>
             <AllStories books={books} />
           </>
         )}
+
         {activeTab === 'archive' && (
           <div className='text-gray-700'>Contenido del Archive...</div>
         )}
+
         {activeTab === 'reading' && (
-          <div className='text-gray-700'>Contenido de Reading Lists...</div>
+          // Aquí renderizas tu componente para Reading Lists
+          <ReadingListView />
         )}
       </div>
     </section>
