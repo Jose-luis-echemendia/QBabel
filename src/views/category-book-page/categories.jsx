c3ab30932720f0008e1253211a5ba6be1a397445
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 11:18:01 -0400

    Update categories.jsx

--- src/views/category-book-page/categories.jsx
+++ src/views/category-book-page/categories.jsx
@@ -4,10 +4,10 @@ import { v4 as uuidi } from "uuid";
 export const Categories = () => {
   return (
     <>
-      <div className="flex flex-wrap justify-center items-center w-full h-full z-10 gap-10">
+      <div className="grid grid-cols-8 justify-start items-center h-full z-10 gap-7 border p-5 rounded-md shadow-md">
         {schemaImagesCategoria.map((category) => (
-          <div key={uuidi()} className="bg-gray-400 py-2 px-5 rounded-2xl">
-            <span className="text-black">{category.text}</span>
+          <div key={uuidi()} className="bg-gray-200 py-2 px-3 rounded-2xl flex items-center justify-center">
+            <span className="text-black text-sm">{category.text}</span>
           </div>
         ))}
       </div>
