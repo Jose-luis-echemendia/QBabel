5648f6040c7b1910e58ae5c11ce1bfc84cd8bf30
Andy Torres <andy18wrld@gmail.com>
2025-03-20 21:58:15 -0400

    Libraryview

--- src/views/library-page/index.jsx
+++ src/views/library-page/index.jsx
@@ -1,8 +1,11 @@
-
+import { bestBooksData } from '../../constants/home-page/best-books';
+import LibrarySection from './library-section';
 const LibraryView = () => {
   return (
-    <>Library</>
-  )
-}
+    <div className='min-h-screen bg-gray-100'>
+      <LibrarySection books={bestBooksData} />
+    </div>
+  );
+};
 
-export default LibraryView  
+export default LibraryView;
