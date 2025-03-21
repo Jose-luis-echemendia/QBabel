3c53d8e198959e4c963675672775b75b49d68c6c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 17:07:02 -0400

    Update best-sellers.jsx

--- src/views/category-book-page/best-sellers.jsx
+++ src/views/category-book-page/best-sellers.jsx
@@ -3,16 +3,11 @@ import { CustomCarouselBooks } from "../home-page/carousel/books";
 export const BestSellers = () => {
   return (
     <>
-      <div className="relative w-full h-full flex flex-col items-start justify-start gap-10">
-        <h3 className="px-20 ml-5 font-semibold text-xl">
+      <div className="w-full h-full flex flex-col container mx-auto items-start justify-start gap-1">
+        <span className="ml-7 text-black font-bold font-opensans text-lg leading-8">
           Top 10 libros más vendidos
-        </h3>
-        <div className="flex flex-col gap-1 container mx-auto">
-          <span className="ml-7 text-black font-bold font-opensans text-lg leading-8">
-            Lo mejor de fantasía para ti
-          </span>
-          <CustomCarouselBooks carouselSize={`h-80`} booksPerSlide={8} />
-        </div>
+        </span>
+        <CustomCarouselBooks carouselSize={`h-80`} booksPerSlide={8} />
       </div>
     </>
   );
