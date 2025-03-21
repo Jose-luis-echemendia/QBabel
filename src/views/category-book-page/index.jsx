b21f63e2a49a710d462163935f25585323c2d21f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 08:20:14 -0400

    Update index.jsx

--- src/views/category-book-page/index.jsx
+++ src/views/category-book-page/index.jsx
@@ -1,5 +1,17 @@
+import { BestSellers } from "./best-sellers";
+import { Categories } from "./categories";
+import { CategoryBooks } from "./category-books";
+
 const CategoryBookView = () => {
-  return <>category</>;
+  return (
+    <>
+      <div className="min-h-screen h-full w-full">
+        <BestSellers/>
+        <Categories/>
+        <CategoryBooks/>
+      </div>
+    </>
+  );
 };
 
 export default CategoryBookView;
