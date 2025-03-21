969e47e56041b46e3ef6929b4b03d816f945dbe3
Andy Torres <andy18wrld@gmail.com>
2025-03-20 22:36:31 -0400

    Agragado una columna mas

--- src/views/library-page/allstories.jsx
+++ src/views/library-page/allstories.jsx
@@ -5,8 +5,8 @@ import BookCard from './book-card';
 function AllStories({ books }) {
   return (
     <section className='py-4'>
-      <div className='max-w-screen-xl'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10  '>
+      <div className='max-w-screen-xl mx-auto'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3'>
           {books.map((book) => (
             <BookCard key={book.id} book={book} />
           ))}
