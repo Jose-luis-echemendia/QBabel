85725a67d3535a06fe456c5c589ef0a0c06edfb8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 08:33:47 -0400

    Update index.jsx

--- src/views/category-book-page/index.jsx
+++ src/views/category-book-page/index.jsx
@@ -6,11 +6,16 @@ const CategoryBookView = () => {
   return (
     <>
       <div className="min-h-screen h-full w-full flex flex-col items-center justify-start px-2 py-10 gap-14 relative">
-        <h2 className="text-3xl font-bold text-center font-italianno">Historias de Romances</h2>
-        <h2 className="font-italianno mx-auto w-fit mt-2 text-[50px] leading-[55px] text-center">Historias de Romances</h2>
-        <BestSellers/>
-        <Categories/>
-        <CategoryBooks/>
+        <div className="absolute right-1/4 mt-5">
+          <h2 className="w-full text-[50px] leading-[55px] font-bold text-center font-italianno">
+            Historias de Romances
+          </h2>
+        </div>
+        <div className="mt-44 w-full h-full flex flex-col items-center justify-start">
+          <BestSellers />
+          <Categories />
+          <CategoryBooks />
+        </div>
       </div>
     </>
   );
