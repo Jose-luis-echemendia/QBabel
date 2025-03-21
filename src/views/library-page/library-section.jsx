2f99a31b6b832bed9e8159ae552868c8b4f0303c
Andy Torres <andy18wrld@gmail.com>
2025-03-20 21:58:06 -0400

    creado section-library

---
+++ src/views/library-page/library-section.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import AllStories from './AllStories';
+
+function LibrarySection({ books }) {
+  const [activeTab, setActiveTab] = useState('current'); 
+
+  return (
+    <section className='py-4 '>
+      <div className='max-w-[1200px] mx-auto px-4 '>
+        
+        <h2 className=' relative -top-10 text-3xl font-bold mb-4 mt-20  '>
+          Library
+        </h2>
+
+        
+        <div className='flex items-center justify-between border-b border-gray-200 mb-4'>
+          
+          <div className='flex space-x-6 text-xl'>
+            <button
+              className={`pb-2 ${
+                activeTab === 'current'
+                  ? 'border-b-2 border-orange-900 font-bold'
+                  : 'text-gray-600'
+              }`}
+              onClick={() => setActiveTab('current')}
+            >
+              Current reads
+            </button>
+            <button
+              className={`pb-2 ${
+                activeTab === 'archive'
+                  ? 'border-b-2 border-orange-900 font-bold'
+                  : 'text-gray-600'
+              }`}
+              onClick={() => setActiveTab('archive')}
+            >
+              Archive
+            </button>
+            <button
+              className={`pb-2 ${
+                activeTab === 'reading'
+                  ? 'border-b-2 border-orange-900 font-bold'
+                  : 'text-gray-600'
+              }`}
+              onClick={() => setActiveTab('reading')}
+            >
+              Reading Lists
+            </button>
+          </div>
+
+         
+          <div className='text-gray-600 text-sm'>Private</div>
+        </div>
+
+       
+        {activeTab === 'current' && (
+          <>
+            <h3 className='text-xl font-bold mb-4'>All Stories</h3>
+            <AllStories books={books} />
+          </>
+        )}
+        {activeTab === 'archive' && (
+          <div className='text-gray-700'>Contenido del Archive...</div>
+        )}
+        {activeTab === 'reading' && (
+          <div className='text-gray-700'>Contenido de Reading Lists...</div>
+        )}
+      </div>
+    </section>
+  );
+}
+
+LibrarySection.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+
+export default LibrarySection;
