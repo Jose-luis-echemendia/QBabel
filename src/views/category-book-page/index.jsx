ba04b340b71373070d1853af3b7ed77bbd6bcace
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 11:09:45 -0400

    Update index.jsx

--- src/views/category-book-page/index.jsx
+++ src/views/category-book-page/index.jsx
@@ -7,11 +7,11 @@ const CategoryBookView = () => {
     <>
       <div className="min-h-screen h-full w-full flex flex-col items-center justify-start px-2 py-10 gap-14 relative">
         <div className="absolute right-1/4 mt-5">
-          <h2 className="w-full text-[50px] leading-[55px] font-bold text-center font-italianno">
+          <h2 className="w-full text-[70px] leading-[80px] text-center font-italianno text-gray-600">
             Historias de Romances
           </h2>
         </div>
-        <div className="mt-44 w-full h-full flex flex-col items-center justify-start gap-14">
+        <div className="mt-32 w-full h-full flex flex-col items-center justify-start gap-14">
           <BestSellers />
           <Categories />
           <CategoryBooks />
