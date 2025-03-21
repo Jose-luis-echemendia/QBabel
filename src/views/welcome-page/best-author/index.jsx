12fff967d265c6301a38f383a8f598c962d58354
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 16:57:39 -0400

    Update index.jsx

--- src/views/welcome-page/best-author/index.jsx
+++ src/views/welcome-page/best-author/index.jsx
@@ -10,7 +10,6 @@ export const BestAuthorPage = () => {
         <p className="mx-auto w-fit text-[20px] font-opensans font-semibold mt-28 text-[#644844]">
           Find out more about what we do for writers →
         </p>
-
         <h2 className="mx-auto w-fit font-quicksand mt-16 font-bold text-[64px] text-[#644844]">
           Escritores destacados
         </h2>
