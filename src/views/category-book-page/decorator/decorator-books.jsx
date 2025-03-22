60b16442c81aa6be33a2f90365fc18bdeee49f4b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 22:42:01 -0400

    Update decorator-books.jsx

--- src/views/category-book-page/decorator/decorator-books.jsx
+++ src/views/category-book-page/decorator/decorator-books.jsx
@@ -7,17 +7,21 @@ const DecoratorBooks = () => {
       {/* LEFT */}
       <CustomImageDecorator
         src="/assets/images/home/explota_talento/9.webp"
-        className="absolute -left-[240px] top-0 z-0 w-[200px] mt-2"
+        className="absolute -left-[220px] -top-[40px] z-0 w-[200px] mt-2"
       />
       <CustomImageDecorator
         src="/assets/images/home/explota_talento/1.webp"
-        className="absolute -left-[200px] top-1/2 z-0 w-[200px] mt-2"
+        className="absolute -left-[180px] top-0 z-0 w-[200px] mt-2"
       />
       <CustomImageDecorator
         src="/assets/images/home/explota_talento/6.webp"
-        className="absolute -left-[240px] z-0 w-[200px] mt-2"
+        className="absolute -left-[220px] top-[100px] z-0 w-[200px] mt-2"
       />
       {/* RIGHT */}
+      <CustomImageDecorator
+        src="/assets/images/home/explota_talento/2.webp"
+        className="absolute -right-[200px] top-0 z-0 w-[200px] mt-2"
+      />
       <CustomImageDecorator
         src="/assets/images/home/explota_talento/11.jpg"
         className="absolute -right-[200px] top-[100px] z-0 w-[120px] mt-2"
