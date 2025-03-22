b4fea57d68cd7acbccb7aa0025650e3e60d78aa3
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 17:45:58 -0400

    Update body-category-books.jsx

--- src/views/category-book-page/category-books/body-category-books.jsx
+++ src/views/category-book-page/category-books/body-category-books.jsx
@@ -1,8 +1,28 @@
 import { booksData } from "@/constants/home-page/best-books";
 
-
 export const BodyCategoryBooks = () => {
   return (
-    <>bodyCategoryBooks</>
-  )
-}
+    <>
+      <div className="gird grid-cols-2 w-full h-full py-3 px-5 place-items-center place-self-center">
+        {booksData.map((book) => (
+          <div
+            key={book.id}
+            className="relative flex flex-col items-center justify-center w-full h-full bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
+          >
+            <img
+              src={book.image}
+              alt={book.title}
+              className="w-full h-auto rounded-t-lg"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold text-gray-800">
+                {book.title}
+              </h3>
+              <p className="text-sm text-gray-600">{book.author}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
