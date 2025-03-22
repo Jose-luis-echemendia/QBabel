34d9af69eb370ca43bd36fa8225d22ebb5a859b8
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 22:15:30 -0400

    Update decorator-books.jsx

--- src/views/category-book-page/decorator/decorator-books.jsx
+++ src/views/category-book-page/decorator/decorator-books.jsx
@@ -5,24 +5,24 @@ export const DecoratorBooks = () => {
     <>
       {/* LEFT */}
       <CustomImageDecorator
-        src="/assets/icons/category-books/1.svg"
+        src="/assets/images/books/1.png"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/icons/category-books/1.svg"
+        src="/assets/images/books/1.png"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/icons/category-books/1.svg"
+        src="/assets/images/books/1.png"
         className="absolute z-0 w-full mt-2"
       />
       {/* RIGHT */}
       <CustomImageDecorator
-        src="/assets/icons/category-books/1.svg"
+        src="/assets/images/books/1.png"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/icons/category-books/1.svg"
+        src="/assets/images/books/1.png"
         className="absolute z-0 w-full mt-2"
       />
     </>
