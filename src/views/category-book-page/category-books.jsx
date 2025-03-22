b7977421f7ba587e498c72e91a3833a6f00d577b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 16:47:25 -0400

    Update category-books.jsx

--- src/views/category-book-page/category-books.jsx
+++ src/views/category-book-page/category-books.jsx
@@ -2,7 +2,7 @@
 export const CategoryBooks = () => {
   return (
     <>
-        <section className="container mx-auto bg-[#C5A9DF] w-full h-full rounded-xl">
+        <section className="container mx-auto bg-[#C5A9DF] w-full h-full rounded-xl mt-20 shadow-lg flex flex-col items-center justify-center">
             <header className="flex flex-col items-center justify-center py-10">
           
             </header>
