6e488a0e5be3df561ae2109b956482d40d79b571
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 19:04:22 -0400

    Update index.jsx

--- src/views/category-book-page/category-books/index.jsx
+++ src/views/category-book-page/category-books/index.jsx
@@ -4,7 +4,7 @@ import { HeaderCategoryBooks } from "./header-category-books";
 export const CategoryBooks = () => {
   return (
     <>
-      <section className="container mx-auto bg-[hsl(271,46%,77%)] bg-opacity-55 w-full h-full rounded-xl mt-20 shadow-lg flex flex-col items-center justify-center">
+      <section className="container mx-auto bg-[hsl(271,46%,77%)] bg-opacity-55 w-full h-full rounded-xl mt-20 shadow-lg flex flex-col items-center justify-center pb-5">
         <HeaderCategoryBooks />
         <div className="w-full px-10">
           <hr className="w-full mx-auto" />
