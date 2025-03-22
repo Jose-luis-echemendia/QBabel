3f70bd3e03560c544f36e25952e53d118d881335
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 18:30:46 -0400

    Update body-category-books.jsx

--- src/views/category-book-page/category-books/body-category-books.jsx
+++ src/views/category-book-page/category-books/body-category-books.jsx
@@ -19,16 +19,17 @@ export const BodyCategoryBooks = () => {
               >
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
               </svg>
-              <span className="flex items-center justify-center absolute top-2 left-4 text-primary font-bold">{book.id} </span>
+              <span className="flex items-center justify-center absolute top-2 left-4 text-primary font-bold">
+                {book.id}{" "}
+              </span>
             </div>
-            
-            <figure>
-              <img
-                src={book.img}
-                alt={book.title}
-                className="w-40 h-auto rounded-t-lg"
-              />
-            </figure>
+
+            <img
+              src={book.img}
+              alt={book.title}
+              className="w-40 h-auto rounded-t-lg"
+            />
+
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-800">
                 {book.title}
