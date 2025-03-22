9e5c620398e852f3d1c3677a8d86b4ba7634c144
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 17:02:14 -0400

    Update category-books.jsx

--- src/views/category-book-page/category-books.jsx
+++ src/views/category-book-page/category-books.jsx
@@ -3,14 +3,15 @@ import { booksData } from "@/constants/home-page/best-books";
 export const CategoryBooks = () => {
   return (
     <>
-      <section className="container mx-auto bg-[#C5A9DF] w-full h-full rounded-xl mt-20 shadow-lg flex flex-col items-center justify-center">
-        <header className="flex flex-col items-center justify-between py-10 px-10">
+      <section className="container mx-auto bg-[hsl(271,46%,77%)] w-full h-full rounded-xl mt-20 shadow-lg flex flex-col items-center justify-center">
+        <header className="flex justify-between py-10 ">
           <div className="relative">
             <div />
             <span>2k Historias</span>
           </div>
-          <div></div>
+          <div>Filtrar por:</div>
         </header>
+        <hr />
         <div></div>
       </section>
     </>
