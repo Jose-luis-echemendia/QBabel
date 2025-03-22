67e80081770da114e1ebce9bbddcf3bda9789178
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-21 22:30:30 -0400

    Update decorator-books.jsx

--- src/views/category-book-page/decorator/decorator-books.jsx
+++ src/views/category-book-page/decorator/decorator-books.jsx
@@ -1,30 +1,34 @@
 import { CustomImageDecorator } from "@/components/image-decorator";
+import { memo } from "react";
 
-export const DecoratorBooks = () => {
+const DecoratorBooks = () => {
   return (
     <>
       {/* LEFT */}
       <CustomImageDecorator
-        src="/assets/images/books/1.png"
+        src="/assets/images/home/explota_talento/9.webp"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/images/books/1.png"
+        src="/assets/images/home/explota_talento/1.webp"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/images/books/1.png"
+        src="/assets/images/home/explota_talento/6.webp"
         className="absolute z-0 w-full mt-2"
       />
       {/* RIGHT */}
       <CustomImageDecorator
-        src="/assets/images/books/1.png"
+        src="/assets/images/home/explota_talento/11.jpg"
         className="absolute z-0 w-full mt-2"
       />
       <CustomImageDecorator
-        src="/assets/images/books/1.png"
+        src="/assets/images/home/explota_talento/8.webp"
         className="absolute z-0 w-full mt-2"
       />
     </>
   );
 };
+
+DecoratorBooks.displayName = "DecoratorBooks";
+export default memo(DecoratorBooks);
