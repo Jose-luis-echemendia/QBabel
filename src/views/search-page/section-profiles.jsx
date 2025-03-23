577171d1c883fc6231339afcfd0b23cc5f8e9d98
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 15:27:39 -0400

    Create section-profiles.jsx

---
+++ src/views/search-page/section-profiles.jsx
@@ -0,0 +1,12 @@
+export const SectionProfiles = () => {
+  return (
+    <>
+      <div className="col-span-2 w-full h-full bg-red-200">dsf</div>
+      <div className="col-span-8 w-full h-full grid grid-cols-2">
+        {
+            
+        }
+      </div>
+    </>
+  );
+};
