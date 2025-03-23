e13b7f95458b5eff4c5e2a6543ddbb040a5336cc
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-22 23:28:49 -0400

    Update index.jsx

--- src/views/search-book-page/index.jsx
+++ src/views/search-book-page/index.jsx
@@ -1,7 +1,11 @@
+import { HeaderSearchBooks } from "./header";
+
 const SearchBookView = () => {
   return (
     <>
-      <div className="min-h-screen h-full w-full flex flex-col items-center justify-start px-2 py-10 gap-14 relative"></div>
+      <div className="min-h-screen h-full w-full flex flex-col items-center justify-start px-2 py-10 gap-14 relative">
+        <HeaderSearchBooks/>
+      </div>
     </>
   );
 };
