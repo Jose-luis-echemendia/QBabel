b7fbbffc6698cd88f0c92a243bbe938e4fc155bd
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 15:37:22 -0400

    Update section-books.jsx

--- src/views/search-page/section-books.jsx
+++ src/views/search-page/section-books.jsx
@@ -62,29 +62,32 @@ export const SectionBooks = () => {
                 <strong>Por: </strong>{" "}
                 <span className="text-gray-700"> {book.author}</span>
               </div>
-              <div className="flex gap-6 mt-3 ml-3 justify-start items-center">
+              <div className="flex gap-6 mt-3 ml-0 justify-start items-center">
                 {!book.isFree && (
-                  <div className="flex flex-col gap-1 items-center justify-center">
-                    <span className="flex gap-1.5  text-gray-800 text-xs">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                        />
-                      </svg>
+                  <>
+                    <div className="flex flex-col gap-1 items-center justify-center px-1.5">
+                      <span className="flex gap-1.5  text-gray-800">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-4"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                          />
+                        </svg>
 
-                      <small>Price</small>
-                    </span>
-                    <span className="text-sm font-bold">{book.price}</span>
-                  </div>
+                        <small>Price</small>
+                      </span>
+                      <span className="text-sm font-bold">{book.price}</span>
+                    </div>
+                    <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
+                  </>
                 )}
                 <div className="flex flex-col gap-1 items-center justify-center">
                   <span className="flex gap-1.5 text-gray-800">
