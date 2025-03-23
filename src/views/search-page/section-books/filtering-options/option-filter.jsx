96d34b55b87c40157b51006b2928316b0450ef8b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 17:10:13 -0400

    Update option-filter.jsx

--- src/views/search-page/section-books/filtering-options/option-filter.jsx
+++ src/views/search-page/section-books/filtering-options/option-filter.jsx
@@ -113,7 +113,25 @@ export const OptionFilter = ({
 }) => {
   return (
     <>
-      <Accordion open={openAcc}>
+      <Accordion
+        open={openAcc}
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        }
+      >
         <div className="flex flex-col gap-0.5 mt-5">
           <AccordionHeader
             onClick={handleOpenAcc}
