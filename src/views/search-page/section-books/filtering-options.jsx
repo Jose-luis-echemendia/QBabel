96be473ce06aa4ff71eb39e4772168aa5736ca6f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 16:00:31 -0400

    Update filtering-options.jsx

--- src/views/search-page/section-books/filtering-options.jsx
+++ src/views/search-page/section-books/filtering-options.jsx
@@ -1,6 +1,11 @@
+import { useParams } from 'react-router-dom';
 
 export const FilteringOptions = () => {
+    const { criterion } = useParams();
   return (
-    <div className="col-span-2 w-full h-full bg-red-200">FilteringOptions</div>
+    <div className="col-span-2 w-full h-full flex flex-col gap-2.5">
+        <h6 className='text-3xl font-bold'>&quot;{criterion}&quot;</h6>
+        <span className='text-sm text-gray-600 -mt-1'>395 resultados</span>
+    </div>
   )
 }
