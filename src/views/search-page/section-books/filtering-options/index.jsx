c5c1a173d4c0003ba805fbdc49d48617f6320108
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 17:07:40 -0400

    Update index.jsx

--- src/views/search-page/section-books/filtering-options/index.jsx
+++ src/views/search-page/section-books/filtering-options/index.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { OptionFilter } from "./option-filter";
+import { useState } from "react";
 
 const partsCriterion = [
   { id: 1, criterion: "Cualquier extension" },
@@ -24,6 +25,7 @@ const contentCriterion = [
   { id: 6, criterion: "Primeras historias" },
 ];
 const priceCriterion = [
+  { id: 0, criterion: "Todos los precios" },
   { id: 1, criterion: "$0 - 100$" },
   { id: 2, criterion: "$100 - 500$" },
   { id: 3, criterion: "$500 - 1500$" },
@@ -32,23 +34,41 @@ const priceCriterion = [
 
 export const FilteringOptions = () => {
   const { criterion } = useParams();
+  const [openAcc1, setOpenAcc1] = useState(true);
+  const [openAcc2, setOpenAcc2] = useState(true);
+  const [openAcc3, setOpenAcc3] = useState(true);
+  const [openAcc4, setOpenAcc4] = useState(true);
+
+  const handleOpenAcc1 = () => setOpenAcc1((cur) => !cur);
+  const handleOpenAcc2 = () => setOpenAcc2((cur) => !cur);
+  const handleOpenAcc3 = () => setOpenAcc3((cur) => !cur);
+  const handleOpenAcc4 = () => setOpenAcc4((cur) => !cur);
+
   return (
     <div className="col-span-2 w-full h-full flex flex-col gap-2.5">
       <h6 className="text-3xl font-bold">&quot;{criterion}&quot;</h6>
       <span className="text-sm text-gray-600 -mt-1">395 resultados</span>
       <OptionFilter
+        openAcc={openAcc1}
+        handleOpenAcc={handleOpenAcc1}
         criterion={"Capítulos"}
         options={partsCriterion}
       />
       <OptionFilter
+        openAcc={openAcc2}
+        handleOpenAcc={handleOpenAcc2}
         criterion={"Última actualización"}
         options={updateCriterion}
       />
       <OptionFilter
+        openAcc={openAcc3}
+        handleOpenAcc={handleOpenAcc3}
         criterion={"Contenido"}
         options={contentCriterion}
       />
       <OptionFilter
+        openAcc={openAcc4}
+        handleOpenAcc={handleOpenAcc4}
         criterion={"Precios"}
         options={priceCriterion}
       />
