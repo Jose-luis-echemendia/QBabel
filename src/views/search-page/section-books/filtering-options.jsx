978e65641a6a1436d97038a0065ac4cffa10986f
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 16:11:06 -0400

    Update filtering-options.jsx

--- src/views/search-page/section-books/filtering-options.jsx
+++ src/views/search-page/section-books/filtering-options.jsx
@@ -6,6 +6,14 @@ export const FilteringOptions = () => {
     <div className="col-span-2 w-full h-full flex flex-col gap-2.5">
         <h6 className='text-3xl font-bold'>&quot;{criterion}&quot;</h6>
         <span className='text-sm text-gray-600 -mt-1'>395 resultados</span>
+        <div className='flex flex-col gap-0.5 mt-5'>
+            <span className='text-xl font-bold'>Capítulos</span>
+            <small className='text-xs text-gray-600'>Puedes seleccionar múltiples opciones</small>
+            <div className='flex justify-between items-center w-full'></div>
+            <div className='flex justify-between items-center w-full'></div>
+            <div className='flex justify-between items-center w-full'></div>
+            <div className='flex justify-between items-center w-full'></div>
+        </div>
     </div>
   )
 }
