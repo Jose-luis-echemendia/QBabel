af58a0ac68f0c035d95bbd61d0dfd036ab3ca271
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-23 15:14:30 -0400

    Create header.jsx

---
+++ src/views/search-page/header.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const HeaderSearchBooks = () => {
+  const [activeTab, setActiveTab] = useState("historias");
+
+  return (
+    <div className="flex items-start justify-between border-b border-gray-200 mb-4">
+      <div className="flex space-x-7 text-4xl">
+        <button
+          className={`pb-2 ${
+            activeTab === "historias"
+              ? "border-b-2 border-primary font-semibold"
+              : "text-gray-600"
+          }`}
+          onClick={() => setActiveTab("current")}
+        >
+          Historias
+        </button>
+        <button
+          className={`pb-2 ${
+            activeTab === "perfiles"
+              ? "border-b-2 border-primary font-bold"
+              : "text-gray-600"
+          }`}
+          onClick={() => setActiveTab("perfiles")}
+        >
+          Perfiles
+        </button>
+      </div>
+    </div>
+  );
+};
