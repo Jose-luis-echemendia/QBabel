de287b604d70a09f2ff52e498ccec6efff1e4e17
Andy Torres <andy18wrld@gmail.com>
2025-03-24 00:33:14 -0400

    Ajustando estilos

--- src/views/library-page/allstories.jsx
+++ src/views/library-page/allstories.jsx
@@ -6,7 +6,7 @@ function AllStories({ books }) {
   return (
     <section className='py-4'>
       <div className='max-w-screen-xl mx-auto'>
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10'>
           {books.map((book) => (
             <BookCard key={book.id} book={book} />
           ))}
