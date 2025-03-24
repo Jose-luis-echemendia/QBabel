7f60e094aec1badf2ecbf6a3f422c98a4d0d1431
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-24 00:01:15 -0400

    Create index.jsx

---
+++ src/views/search-page/section-profiles/index.jsx
@@ -0,0 +1,12 @@
+import { Books } from "../section-books/books";
+import { FilteringOptions } from "../section-books/filtering-options";
+
+export const SectionProfiles = () => {
+    return (
+      <>
+        <FilteringOptions />
+        <Books />
+      </>
+    );
+  };
+  
