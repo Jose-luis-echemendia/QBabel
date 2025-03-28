77c2674f3a55182556514afef51bfae37edefb9b
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-28 10:32:45 -0400

    Update index.jsx

--- src/views/admin-page/admin-books/overview-book/index.jsx
+++ src/views/admin-page/admin-books/overview-book/index.jsx
@@ -4,13 +4,26 @@ export const OverViewBook = ({ book }) => {
   return (
     <>
       <section className="p-5 grid grid-cols-11">
-        <div className="col-span-4 bg-blue-200">
-          <CoverGalleries
-            covers={book.covers}
-            alt={book.title}
-          ></CoverGalleries>
+        <div className="col-span-4">
+          <CoverGalleries covers={book.covers} alt={book.title} />
         </div>
-        <div className="col-span-7 bg-red-200">sdf</div>
+        <div className="col-span-7 flex flex-col gap-3 leading-8">
+          <h3 className="text-3xl font-semibold text-black">{book.title}</h3>
+          <span className="text-gray-700 font-semibold">{book.author.name} </span>
+          <p className="line-clamp-5 text-sm text-balance text-justify">
+            {book.sumary} Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Suscipit iste aliquid magni eos, dicta eligendi voluptates
+            aspernatur eaque corrupti, incidunt iusto! Blanditiis quibusdam, eum
+            enim ipsum libero asperiores eius tenetur! Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Dolorem laborum unde placeat
+            eaque neque accusamus excepturi dolor doloribus aliquid! Accusantium
+            perspiciatis similique, nobis cum fugiat debitis quod ipsum ipsam
+            eligendi! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Odit id accusantium et, reiciendis labore nisi fuga distinctio
+            tenetur nesciunt voluptatibus ullam totam. Ut reiciendis rerum ea
+            similique ratione voluptatem corporis.
+          </p>
+        </div>
       </section>
     </>
   );
