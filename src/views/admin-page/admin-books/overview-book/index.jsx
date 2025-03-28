fec0ac6733862ad48001cbc4494ff9e261957dfe
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-28 10:47:07 -0400

    Update index.jsx

--- src/views/admin-page/admin-books/overview-book/index.jsx
+++ src/views/admin-page/admin-books/overview-book/index.jsx
@@ -3,13 +3,36 @@ import { CoverGalleries } from "./cover-galleries";
 export const OverViewBook = ({ book }) => {
   return (
     <>
-      <section className="p-5 grid grid-cols-11">
+      <section className="p-5 grid grid-cols-11 gap-3">
         <div className="col-span-4 h-full">
           <CoverGalleries covers={book.covers} alt={book.title} />
         </div>
         <div className="col-span-7 relative flex flex-col gap-3 leading-8">
           <h3 className="text-3xl font-semibold text-black">{book.title}</h3>
-          <span className="text-gray-700 font-semibold">{book.author.name} </span>
+          <span className="text-gray-700 font-semibold">
+            {book.author.name}
+          </span>
+          <div className="flex gap-2">
+            {book.isFree ? (
+              <div className="p-1 h-8 flex items-center rounded-xl bg-green-800 -ml-[1px]">
+                <span className="text-white-100 text-sm font-semibold">Gratuito!</span>
+              </div>
+            ) : (
+              <div className="p-1 h-8 flex items-center rounded-xl bg-gray-cam -ml-[1px]">
+                <span className="text-white-100 text-sm font-semibold">De Pago</span>
+              </div>
+            )}
+
+            {book.isComplete ? (
+              <div className="p-1 h-8 flex items-center rounded-xl bg-green-800 -ml-[1px]">
+                <span className="text-white-100 text-sm font-semibold">Completada</span>
+              </div>
+            ) : (
+              <div className="p-1 h-8 flex items-center rounded-xl bg-gray-cam -ml-[1px]">
+                <span className="text-white-100 text-sm font-semibold">En curso</span>
+              </div>
+            )}
+          </div>
           <p className="line-clamp-5 text-sm text-balance text-justify">
             {book.sumary} Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Suscipit iste aliquid magni eos, dicta eligendi voluptates
@@ -24,18 +47,18 @@ export const OverViewBook = ({ book }) => {
             similique ratione voluptatem corporis.
           </p>
           <span>{book.license}</span>
-        <div className="flex flex-wrap gap-2">
-          {book.categories.map((category) => (
-            <span
-              key={category.id}
-              className="px-3 py-1.5 rounded-2xl bg-gray-200 text-black font-semibold"
-            >
-              {category.name}
-            </span>
-          ))}
+          <div className="flex flex-wrap gap-3">
+            {book.categories.map((category) => (
+              <span
+                key={category.id}
+                className="px-2.5 py-1 flex items-center h-8 rounded-2xl bg-gray-200 text-black font-semibold"
+              >
+                {category.name}
+              </span>
+            ))}
+          </div>
         </div>
-        </div>
-        <div className="col-span-11 flex items-center justify-center">
+        <div className="col-span-11 flex items-center justify-center mt-4">
           tablita
         </div>
       </section>
