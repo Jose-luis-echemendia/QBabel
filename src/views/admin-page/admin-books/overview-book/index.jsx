4df979e017eb3b87dd6f0f5c4f003adc1a48b263
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-27 23:37:38 -0400

    Update index.jsx

--- src/views/admin-page/admin-books/overview-book/index.jsx
+++ src/views/admin-page/admin-books/overview-book/index.jsx
@@ -1,23 +1,11 @@
+import { CoverGalleries } from "./cover-galleries";
+
 export const OverViewBook = ({ book }) => {
-    return (
-      <>
-        <section className="p-5">
-          <div className="text-center w-56">
-            <div className="mx-auto my-4 w-48">
-              <h3 className="text-lg font-black text-gray-800">
-                this item is already in cart
-              </h3>
-              <p className="text-sm text-gray-500">
-                Do you want to check the shopping cart?
-              </p>
-            </div>
-            <div className="flex gap-4">
-              <button className="btn btn-danger w-full">Continue</button>
-              <button className="btn btn-light w-full">Cancel</button>
-            </div>
-          </div>
-        </section>
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <section className="p-5">
+        <CoverGalleries covers={book.covers} alt={book.title}></CoverGalleries>
+      </section>
+    </>
+  );
+};
