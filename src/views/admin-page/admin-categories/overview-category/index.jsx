411c87e326096aaa665325eeb64eafd0ea07338d
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-29 18:32:34 -0400

    Update index.jsx

--- src/views/admin-page/admin-categories/overview-category/index.jsx
+++ src/views/admin-page/admin-categories/overview-category/index.jsx
@@ -4,6 +4,26 @@ export const OverViewCategory = ({ category }) => {
       <div className="flex flex-col gap-4 items-center justify-center w-full h-full p-5">
         <h4 className="text-black font-semibold text-2xl w-fit">Category</h4>
         <form action="" className="grid grid-cols-6 w-full h-full">
+        <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Nombre
+                </label>
+                <div className="mt-2">
+                  <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      placeholder="janesmith"
+                      className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+              </div>
+          
           <div className="col-span-full">
             <label
               htmlFor="cover-photo"
