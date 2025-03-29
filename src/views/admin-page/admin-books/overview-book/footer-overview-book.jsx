3eed3e5a8220674d4fc8893505389c32f3990576
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-03-28 22:17:32 -0400

    Update footer-overview-book.jsx

--- src/views/admin-page/admin-books/overview-book/footer-overview-book.jsx
+++ src/views/admin-page/admin-books/overview-book/footer-overview-book.jsx
@@ -1,100 +1,107 @@
 export const FooterOverviewBook = ({ book }) => {
   return (
     <>
-      <div className="flex gap-10 items-center justify-center">
-        {!book.isFree && (
-          <>
-            <div className="flex flex-col gap-2 items-center justify-center px-1.5">
-              <span className="flex gap-1.5  text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div className="flex gap-10 items-center justify-center">
+          {!book.isFree && (
+            <>
+              <div className="flex flex-col gap-2 items-center justify-center px-1.5">
+                <span className="flex gap-1.5  text-gray-800">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
 
-                <small className="text-lg">Price</small>
-              </span>
-              <span className="text-xl font-bold">{book.price}</span>
-            </div>
-            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-          </>
-        )}
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <span className="flex gap-1.5 text-gray-800">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
-            <small className="text-lg">Lecturas</small>
-          </span>
-          <span className="text-xl font-bold">{book.reads}</span>
-        </div>
-        <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <span className="flex gap-1.5 text-gray-800">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-              />
-            </svg>
-            <small className="text-lg">Votos</small>
-          </span>
-          <span className="text-xl font-bold">{book.reviews}</span>
-        </div>
-        <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <span className="flex gap-1.5 text-gray-800">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-              />
-            </svg>
+                  <small className="text-lg">Price</small>
+                </span>
+                <span className="text-xl font-bold">{book.price}</span>
+              </div>
+              <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
+            </>
+          )}
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <span className="flex gap-1.5 text-gray-800">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+              <small className="text-lg">Lecturas</small>
+            </span>
+            <span className="text-xl font-bold">{book.reads}</span>
+          </div>
+          <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <span className="flex gap-1.5 text-gray-800">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                />
+              </svg>
+              <small className="text-lg">Votos</small>
+            </span>
+            <span className="text-xl font-bold">{book.reviews}</span>
+          </div>
+          <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <span className="flex gap-1.5 text-gray-800">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                />
+              </svg>
 
-            <small className="text-lg">Capítulos</small>
-          </span>
-          <span className="text-xl font-bold">{book.parts}</span>
+              <small className="text-lg">Capítulos</small>
+            </span>
+            <span className="text-xl font-bold">{book.parts}</span>
+          </div>
+        </div>
+        <div>
+          <button className="bg-primary py-2.5 px-16 rounded-full">
+            <span className="font-semibold text-black-80 hover:text-gray-800">Ver Detalles</span>
+          </button>
         </div>
       </div>
     </>
