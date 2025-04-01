0be4f918c5c66dfd12d01cf26e34a8228f916a4c
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-04-01 10:29:22 -0400

    Update index.jsx

--- src/views/admin-page/admin-complaints/index.jsx
+++ src/views/admin-page/admin-complaints/index.jsx
@@ -1,5 +1,5 @@
 const AdminComplaintsView = () => {
-  return <div className="bg-gray-50 min-h-[80vh]">index</div>;
+  return <div className="bg-gray-50 min-h-[80vh] py-3 px-5">index</div>;
 };
 
 export default AdminComplaintsView;
