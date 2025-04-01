af50a141db854bbedc54131cab37c5a567cce1ff
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2025-04-01 16:38:21 -0400

    Update index.jsx

--- src/views/admin-page/admin-sales/index.jsx
+++ src/views/admin-page/admin-sales/index.jsx
@@ -379,6 +379,7 @@ const AdminSalesView = () => {
         <div className="flex flex-col gap-10">
           <HeroProfile />
           <div className="flex gap-10 items-center justify-center">
+            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
             <div className="flex flex-col gap-2 items-center justify-center">
               <span className="flex gap-1.5 text-gray-800">
                 <svg
@@ -392,104 +393,14 @@ const AdminSalesView = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
                   />
                 </svg>
-                <small className="text-lg">Lecturas</small>
+                <small className="text-lg">Compras</small>
               </span>
               <span className="text-xl font-bold">2</span>
             </div>
             <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                  />
-                </svg>
-                <small className="text-lg">Votos</small>
-              </span>
-              <span className="text-xl font-bold">8</span>
-            </div>
-            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-                  />
-                </svg>
-
-                <small className="text-lg">Capítulos</small>
-              </span>
-              <span className="text-xl font-bold">7</span>
-            </div>
-            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-                  />
-                </svg>
-
-                <small className="text-lg">Registrado</small>
-              </span>
-              <span className="text-xl font-bold">4</span>
-            </div>
-            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-                <small className="text-lg">Actualizado</small>
-              </span>
-              <span className="text-xl font-bold">45</span>
-            </div>
           </div>
         </div>
       </CustomModal>
@@ -505,31 +416,6 @@ const AdminSalesView = () => {
         <div className="flex flex-col gap-10">
           <HeroProfile />
           <div className="flex gap-10 items-center justify-center">
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-                <small className="text-lg">Lecturas</small>
-              </span>
-              <span className="text-xl font-bold">2</span>
-            </div>
             <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
             <div className="flex flex-col gap-2 items-center justify-center">
               <span className="flex gap-1.5 text-gray-800">
@@ -544,34 +430,13 @@ const AdminSalesView = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                  />
-                </svg>
-                <small className="text-lg">Votos</small>
-              </span>
-              <span className="text-xl font-bold">8</span>
-            </div>
-            <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
                   />
                 </svg>
 
-                <small className="text-lg">Capítulos</small>
+                <small className="text-lg">Ventas del item</small>
               </span>
-              <span className="text-xl font-bold">7</span>
+              <span className="text-xl font-bold">10</span>
             </div>
             <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
             <div className="flex flex-col gap-2 items-center justify-center">
@@ -587,35 +452,15 @@ const AdminSalesView = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
                   />
                 </svg>
 
-                <small className="text-lg">Registrado</small>
+                <small className="text-lg">Ventas de otros items</small>
               </span>
-              <span className="text-xl font-bold">4</span>
+              <span className="text-xl font-bold">10</span>
             </div>
             <div className="w-[1px] bg-gray-400 -mx-3 h-14" />
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <span className="flex gap-1.5 text-gray-800">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-                <small className="text-lg">Actualizado</small>
-              </span>
-              <span className="text-xl font-bold">45</span>
-            </div>
           </div>
         </div>
       </CustomModal>
