b5079aa6fb239955c5cbb22b2361d09386034381
Jose-luis-echemendia <echemendiajoseluis@gmail.com>
2025-05-15 01:05:31 -0400

    create blog app

--- src/helpers/yup-schemas.js
+++ src/helpers/yup-schemas.js
@@ -1,6 +1,28 @@
 import * as yup from "yup";
 
 export const schemaLogin = yup.object({
-  email: yup.string().email("Email inválido").required("El email es obligatorio"),
-  password: yup.string().min(6, "La contraseña debe tener al menos 6 caracteres").required("La contraseña es obligatoria"),
+  email: yup
+    .string()
+    .email("Email inválido")
+    .required("El email es obligatorio"),
+  password: yup
+    .string()
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .required("La contraseña es obligatoria"),
+});
+
+export const schemaSignup = yup.object({
+  userName: yup.string().required("El nombre es obligatorio"),
+  email: yup
+    .string()
+    .email("Email inválido")
+    .required("El email es obligatorio"),
+  password: yup
+    .string()
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .required("La contraseña es obligatoria"),
+  rePassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Las contraseñas deben coincidir")
+    .required("La confirmación de la contraseña es obligatoria"),
 });
