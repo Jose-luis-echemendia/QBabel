2f8d9b41c77e11e6d4ddb6f53ae66f922f890d51
Andy Torres <andy18wrld@gmail.com>
2025-05-21 23:46:25 -0400

    WIP: guardando cambios antes de merge

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,89 +1,112 @@
-import axiosInstance from "./axiosInstance";
+import axiosInstance from './axiosInstance';
 
 // endpoint for get users
 export const getUsersAPI = async () => {
-    try {
-        const response = await axiosInstance.get("/api/custom-users/",);
-        
-        return response;
-      } catch (error) {
-        console.error("Error en obtener los usuarios:", error.response?.data || error.message);
-        throw error;
-      }
-}
+  try {
+    const response = await axiosInstance.get('/api/custom-users/');
+
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en obtener los usuarios:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for get user by id
 export const getUserByIdAPI = async (id) => {
   try {
-      const response = await axiosInstance.get(`/api/custom-users/${id}/`);
-      
-      return response;
-    } catch (error) {
-      console.error("Error en obtener el usuario:", error.response?.data || error.message);
-      throw error;
-    }
-}
+    const response = await axiosInstance.get(`/api/custom-users/${id}/`);
+
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en obtener el usuario:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for create user
 export const createUserAPI = async (data) => {
-    try {
-        const response = await axiosInstance.post("/api/custom-users/", data);
-        
-        return response;
-      } catch (error) {
-        console.error("Error en crear el usuario:", error.response?.data || error.message);
-        throw error;
-      }
-} 
+  try {
+    const response = await axiosInstance.post('/api/user-register/', data);
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en crear el usuario:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for update user
 export const updateUserAPI = async (data) => {
-    try {
-        const response = await axiosInstance.put(`/api/custom-users/${data.id}/`, data);
-        
-        return response;
-      } catch (error) {
-        console.error("Error en actualizar el usuario:", error.response?.data || error.message);
-        throw error;
-      }
-}
+  try {
+    const response = await axiosInstance.put(
+      `/api/custom-users/${data.id}/`,
+      data
+    );
+
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en actualizar el usuario:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for update partial user
 export const updatePartialUserAPI = async (data) => {
-    try {
-        const response = await axiosInstance.patch(`/api/custom-users/${data.id}/`, data);
-        
-        return response;
-      } catch (error) {
-        console.error("Error en actualizar el usuario:", error.response?.data || error.message);
-        throw error;
-      }
-}
+  try {
+    const response = await axiosInstance.patch(
+      `/api/custom-users/${data.id}/`,
+      data
+    );
+
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en actualizar el usuario:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for delete user
 export const deleteUserAPI = async (id) => {
-    try {
-        const response = await axiosInstance.delete(`/api/custom-users/${id}/`);
-        
-        return response;
-      } catch (error) {
-        console.error("Error en eliminar el usuario:", error.response?.data || error.message);
-        throw error;
-      }
-}
+  try {
+    const response = await axiosInstance.delete(`/api/custom-users/${id}/`);
+
+    return response;
+  } catch (error) {
+    console.error(
+      'Error en eliminar el usuario:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 // endpoint for get authenticated user
 export const getAuthenticatedUserAPI = async () => {
   try {
-    const accessToken = localStorage.getItem("jwtTokenAccess");
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
+    const accessToken = localStorage.getItem('jwtTokenAccess');
+    const isAuthenticated = localStorage.getItem('isAuthenticated');
 
-    if (!accessToken || !isAuthenticated) return
-    const response = await axiosInstance.get("/api/users/me/");
+    if (!accessToken || !isAuthenticated) return;
+    const response = await axiosInstance.get('/api/users/me/');
     return response;
   } catch (error) {
     console.error(
-      "Error al obtener el usuario authenticado:",
+      'Error al obtener el usuario authenticado:',
       error.response?.data || error.message
     );
     throw error;
--- src/helpers/yup-schemas.js
+++ src/helpers/yup-schemas.js
@@ -1,53 +1,53 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const schemaLogin = yup.object({
   email: yup
     .string()
-    .email("Email inválido")
-    .required("El email es obligatorio"),
+    .email('Email inválido')
+    .required('El email es obligatorio'),
   password: yup
     .string()
-    .min(6, "La contraseña debe tener al menos 6 caracteres")
-    .required("La contraseña es obligatoria"),
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    .required('La contraseña es obligatoria'),
 });
 
 export const schemaSignup = yup.object({
-  userName: yup.string().required("El nombre es obligatorio"),
+  user_name: yup.string().required('El nombre es obligatorio'),
   email: yup
     .string()
-    .email("Email inválido")
-    .required("El email es obligatorio"),
+    .email('Email inválido')
+    .required('El email es obligatorio'),
   password: yup
     .string()
-    .min(6, "La contraseña debe tener al menos 6 caracteres")
-    .required("La contraseña es obligatoria"),
-  rePassword: yup
+    .min(8, 'La contraseña debe tener al menos 8 caracteres')
+    .required('La contraseña es obligatoria'),
+  re_password: yup
     .string()
-    .oneOf([yup.ref("password"), null], "Las contraseñas deben coincidir")
-    .required("La confirmación de la contraseña es obligatoria"),
+    .oneOf([yup.ref('password'), null], 'Las contraseñas deben coincidir')
+    .required('La confirmación de la contraseña es obligatoria'),
 });
 
 export const schemaCategory = yup.object({
-  name: yup.string().required("El nombre es obligatorio"),
+  name: yup.string().required('El nombre es obligatorio'),
   description: yup.string(),
   img: yup
     .mixed()
-    .test("fileSize", "El archivo es muy grande", (value) => {
+    .test('fileSize', 'El archivo es muy grande', (value) => {
       if (!value || value.length === 0) return true;
       return value[0].size <= 10 * 1024 * 1024; // 10MB
     })
-    .test("fileType", "El archivo debe ser una imagen", (value) => {
+    .test('fileType', 'El archivo debe ser una imagen', (value) => {
       if (!value || value.length === 0) return true;
       return (
-        value[0].type === "image/jpeg" ||
-        value[0].type === "image/png" ||
-        value[0].type === "image/jpg"
+        value[0].type === 'image/jpeg' ||
+        value[0].type === 'image/png' ||
+        value[0].type === 'image/jpg'
       );
     }),
 
   type: yup
     .string()
-    .oneOf(["Libro", "Revista", "Publicaciones"], "Tipo inválido")
-    .required("El tipo es obligatorio"),
-  isActive: yup.boolean().required("El estado es obligatorio"),
+    .oneOf(['Libro', 'Revista', 'Publicaciones'], 'Tipo inválido')
+    .required('El tipo es obligatorio'),
+  isActive: yup.boolean().required('El estado es obligatorio'),
 });
