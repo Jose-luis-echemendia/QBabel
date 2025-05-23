a6f8d5e91e49424886d117894ef2260da0ea91bd
Jose-luis-echemendia <echemendiajoseluis@gmail.com>
2025-05-23 00:16:48 -0400

    ok

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,17 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from apps.utils.models.models import BaseModel
 from apps.book.models import Book
+
+User = get_user_model()
+
 
 class PurchaseInvoices(BaseModel):
     """
     Model representing purchase invoices.
     """
-    
+
     book = models.ForeignKey(
         Book,
         on_delete=models.PROTECT,
         related_name="purchase_invoices",
         verbose_name="Book",
+    )
+    buyer = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name="purchase_invoices",
+        verbose_name="Buyer",
+    )
+
+    profit = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        verbose_name="Profit",
+    )
+
+    writer_profit = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        verbose_name="Writer Profit",
+    )
+
+    final_payment = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        verbose_name="Final Payment",
     )
 
     def __str__(self):
