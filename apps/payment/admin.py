98f55c6c911371b07641d3602e69efd4efa85a45
Jose-luis-echemendia <echemendiajoseluis@gmail.com>
2025-05-23 03:10:16 -0400

    ok

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import PurchaseInvoices
 
-# Register your models here.
+class PurchaseInvoicesAdmin(admin.ModelAdmin):
+    list_display = (
+        "book",
+        "buyer",
+        "profit",
+        "writer_profit",
+        "final_payment",
+        "created_at",
+    )
+    search_fields = ("book__title", "buyer__username")
+    
+
+admin.site.register(PurchaseInvoices, PurchaseInvoicesAdmin)
